b3fdc44fc757f08bf4d5e77b08f452cd9ac45c3c
Yanzhen Yu <yanzhen@smartx.com>
2018-10-11 16:44:38 +0800

    update the bundle config

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,19 @@ import typescript from 'rollup-plugin-typescript';
 export default {
   input: './src/index.ts',
   plugins: [typescript()],
-  output: {
-    name: 'rrweb',
-    format: 'iife',
-  },
+  output: [
+    {
+      format: 'cjs',
+      file: './dist/index.js',
+    },
+    {
+      format: 'esm',
+      file: './dist/module.js',
+    },
+    {
+      name: 'rrwebSnapshot',
+      format: 'iife',
+      file: './dist/browser.js',
+    },
+  ],
 };
