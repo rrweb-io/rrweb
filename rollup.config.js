ee06fbcda410331ed25374c77b03c6bfce946f04
Yanzhen Yu <yanzhen@smartx.com>
2018-10-15 17:26:10 +0800

    update rollup config so we can benefit from tree shaking

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,43 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 
-export default {
-  input: './src/index.ts',
-  plugins: [typescript(), resolve()],
-  output: [
-    {
-      format: 'cjs',
-      file: './dist/index.js',
-    },
-    {
-      format: 'esm',
-      file: './dist/module.js',
-    },
-    {
-      name: 'rrweb',
-      format: 'iife',
-      file: './dist/browser.js',
-    },
-  ],
-};
+export default [
+  {
+    input: './src/record/index.ts',
+    plugins: [typescript(), resolve()],
+    output: [
+      {
+        format: 'cjs',
+        file: './dist/record/index.js',
+      },
+      {
+        format: 'esm',
+        file: './dist/record/module.js',
+      },
+      {
+        name: 'record',
+        format: 'iife',
+        file: './dist/record/browser.js',
+      },
+    ],
+  },
+  {
+    input: './src/replay/index.ts',
+    plugins: [typescript(), resolve()],
+    output: [
+      {
+        format: 'cjs',
+        file: './dist/replay/index.js',
+      },
+      {
+        format: 'esm',
+        file: './dist/replay/module.js',
+      },
+      {
+        name: 'replay',
+        format: 'iife',
+        file: './dist/replay/browser.js',
+      },
+    ],
+  },
+];
