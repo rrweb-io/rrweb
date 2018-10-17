2eed038e6b3c63d022ea751f742c5fd03b67b40d
Yanzhen Yu <yanzhen@smartx.com>
2018-10-17 12:06:18 +0800

    init repo

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'public/bundle.js'
+	},
+	plugins: [
+		svelte({
+			// opt in to v3 behaviour today
+			skipIntroByDefault: true,
+			nestedTransitions: true,
+
+			// enable run-time checks when not in production
+			dev: !production,
+			// we'll extract any component CSS out into
+			// a separate file — better for performance
+			css: css => {
+				css.write('public/bundle.css');
+			}
+		}),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration —
+		// consult the documentation for details:
+		// https://github.com/rollup/rollup-plugin-commonjs
+		resolve(),
+		commonjs(),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	]
+};
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import App from './App.html';
+
+const app = new App({
+	target: document.body,
+	data: {
+		name: 'world'
+	}
+});
+
+export default app;
