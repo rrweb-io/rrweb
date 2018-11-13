4287fb4810e4c7710fafc8055cd9df0f3ef21bb7
Yanzhen Yu <yanzhen@smartx.com>
2018-11-13 19:10:34 +0800

    update bundle config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,65 @@
 import typescript from 'rollup-plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
 
-export default {
-  input: './src/index.ts',
-  plugins: [typescript()],
-  output: [
-    {
-      format: 'cjs',
-      file: './dist/index.js',
-    },
-    {
-      format: 'esm',
-      file: './dist/module.js',
-    },
-    {
-      name: 'rrwebSnapshot',
-      format: 'iife',
-      file: './dist/browser.js',
-    },
-  ],
-};
+function toMinPath(path) {
+  return path.replace(/\.js$/, '.min.js');
+}
+
+export default [
+  // browser
+  {
+    input: './src/index.ts',
+    plugins: [typescript()],
+    output: [
+      {
+        name: 'rrwebSnapshot',
+        format: 'umd',
+        file: pkg.unpkg,
+      },
+    ],
+  },
+  {
+    input: './src/index.ts',
+    plugins: [typescript(), terser()],
+    output: [
+      {
+        name: 'rrwebSnapshot',
+        format: 'umd',
+        file: toMinPath(pkg.unpkg),
+      },
+    ],
+  },
+  // CommonJS
+  {
+    input: './src/index.ts',
+    plugins: [typescript()],
+    output: [
+      {
+        format: 'cjs',
+        file: pkg.main,
+      },
+    ],
+  },
+  // ES module
+  {
+    input: './src/index.ts',
+    plugins: [typescript()],
+    output: [
+      {
+        format: 'esm',
+        file: pkg.module,
+      },
+    ],
+  },
+  {
+    input: './src/index.ts',
+    plugins: [typescript(), terser()],
+    output: [
+      {
+        format: 'esm',
+        file: toMinPath(pkg.module),
+      },
+    ],
+  },
+];
