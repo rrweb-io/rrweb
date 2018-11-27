5d3d27f9f75e9f24d9243d0aad6942a8fc104b86
Yanzhen Yu <yanzhen@smartx.com>
2018-11-27 17:55:20 +0800

    add rollup postcss plugin and import style from rrweb

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -37,6 +38,8 @@ export default [
     // https://github.com/rollup/rollup-plugin-commonjs
     resolve(),
     commonjs(),
+
+    postcss(),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
