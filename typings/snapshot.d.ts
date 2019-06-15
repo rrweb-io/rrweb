0865c829488cf8caa444e6221ab4e84040fc74a1
Yanzhen Yu <yanzhen@smartx.com>
2019-06-15 14:41:47 +0800

    Release 0.7.12

--- typings/snapshot.d.ts
+++ typings/snapshot.d.ts
@@ -1,5 +1,5 @@
 import { serializedNodeWithId, INode, idNodeMap } from './types';
 export declare function absoluteToStylesheet(cssText: string, href: string): string;
-export declare function serializeNodeWithId(n: Node | INode, doc: Document, map: idNodeMap, blockClass: string | RegExp, skipChild?: boolean, inlineStylesheet?: boolean): serializedNodeWithId | null;
-declare function snapshot(n: Document, blockClass?: string | RegExp, inlineStylesheet?: boolean): [serializedNodeWithId | null, idNodeMap];
+export declare function serializeNodeWithId(n: Node | INode, doc: Document, map: idNodeMap, blockClass: string | RegExp, skipChild?: boolean, inlineStylesheet?: boolean, maskAllInputs?: boolean): serializedNodeWithId | null;
+declare function snapshot(n: Document, blockClass?: string | RegExp, inlineStylesheet?: boolean, maskAllInputs?: boolean): [serializedNodeWithId | null, idNodeMap];
 export default snapshot;
