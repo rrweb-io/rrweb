d90e1671c181a0fc0b74b9e3185fce064ea34d8b
Yanzhen Yu <yanzhen@smartx.com>
2019-08-11 14:41:23 +0800

    Release 0.7.21

--- typings/utils.d.ts
+++ typings/utils.d.ts
@@ -2,7 +2,7 @@ import { Mirror, throttleOptions, listenerHandler, hookResetter, blockClass } fr
 import { INode } from 'rrweb-snapshot';
 export declare function on(type: string, fn: EventListenerOrEventListenerObject, target?: Document | Window): listenerHandler;
 export declare const mirror: Mirror;
-export declare function throttle<T>(func: (arg: T) => void, wait: number, options?: throttleOptions): () => void;
+export declare function throttle<T>(func: (arg: T) => void, wait: number, options?: throttleOptions): (arg: T) => void;
 export declare function hookSetter<T>(target: T, key: string | number | symbol, d: PropertyDescriptor, isRevoked?: boolean): hookResetter;
 export declare function getWindowHeight(): number;
 export declare function getWindowWidth(): number;
