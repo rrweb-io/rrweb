a95759fc9c5cfe271424f5bce2a849c10afb1df7
Yanzhen Yu <yanzhen@smartx.com>
2019-08-11 14:39:26 +0800

    hotfix throttle function args

--- src/utils.ts
+++ src/utils.ts
@@ -53,13 +53,14 @@ export function throttle<T>(
   let timeout: number | null = null;
   let previous = 0;
   // tslint:disable-next-line: only-arrow-functions
-  return function(args: T) {
+  return function(arg: T) {
     let now = Date.now();
     if (!previous && options.leading === false) {
       previous = now;
     }
     let remaining = wait - (now - previous);
     let context = this;
+    let args = arguments;
     if (remaining <= 0 || remaining > wait) {
       if (timeout) {
         window.clearTimeout(timeout);
