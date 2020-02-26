4118d32601641d19b2fda94e0bb813aed7ba5551
Yanzhen Yu <yanzhen@smartx.com>
2020-02-26 09:56:49 +0800

    Release 0.7.25

--- typings/snapshot.d.ts
+++ typings/snapshot.d.ts
@@ -1,5 +1,5 @@
 import { serializedNodeWithId, INode, idNodeMap } from './types';
-export declare function absoluteToStylesheet(cssText: string, href: string): string;
+export declare function absoluteToStylesheet(cssText: string | null, href: string): string;
 export declare function absoluteToDoc(doc: Document, attributeValue: string): string;
 export declare function transformAttribute(doc: Document, name: string, value: string): string;
 export declare function serializeNodeWithId(n: Node | INode, doc: Document, map: idNodeMap, blockClass: string | RegExp, skipChild?: boolean, inlineStylesheet?: boolean, maskAllInputs?: boolean): serializedNodeWithId | null;
