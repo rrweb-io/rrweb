c206e100f1845b73d3fc389c95c212059ac008b9
Yanzhen Yu <yanzhen@smartx.com>
2020-07-18 13:32:36 +0800

    Release 0.7.28

--- typings/snapshot.d.ts
+++ typings/snapshot.d.ts
@@ -1,7 +1,7 @@
-import { serializedNodeWithId, INode, idNodeMap } from './types';
+import { serializedNodeWithId, INode, idNodeMap, MaskInputOptions } from './types';
 export declare function absoluteToStylesheet(cssText: string | null, href: string): string;
 export declare function absoluteToDoc(doc: Document, attributeValue: string): string;
 export declare function transformAttribute(doc: Document, name: string, value: string): string;
-export declare function serializeNodeWithId(n: Node | INode, doc: Document, map: idNodeMap, blockClass: string | RegExp, skipChild?: boolean, inlineStylesheet?: boolean, maskAllInputs?: boolean): serializedNodeWithId | null;
-declare function snapshot(n: Document, blockClass?: string | RegExp, inlineStylesheet?: boolean, maskAllInputs?: boolean): [serializedNodeWithId | null, idNodeMap];
+export declare function serializeNodeWithId(n: Node | INode, doc: Document, map: idNodeMap, blockClass: string | RegExp, skipChild?: boolean, inlineStylesheet?: boolean, maskInputOptions?: MaskInputOptions): serializedNodeWithId | null;
+declare function snapshot(n: Document, blockClass: string | RegExp | undefined, inlineStylesheet: boolean | undefined, maskAllInputsOrOptions: boolean | MaskInputOptions): [serializedNodeWithId | null, idNodeMap];
 export default snapshot;
--- typings/types.d.ts
+++ typings/types.d.ts
@@ -53,3 +53,18 @@ export interface INode extends Node {
 export declare type idNodeMap = {
     [key: number]: INode;
 };
+export declare type MaskInputOptions = Partial<{
+    color: boolean;
+    date: boolean;
+    'datetime-local': boolean;
+    email: boolean;
+    month: boolean;
+    number: boolean;
+    range: boolean;
+    search: boolean;
+    tel: boolean;
+    text: boolean;
+    time: boolean;
+    url: boolean;
+    week: boolean;
+}>;
