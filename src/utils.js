d92bc733df2167d9692a748e7d5c44a8033e7ea0
Jin <jin_wentao@qq.com>
2020-07-25 17:14:39 +0800

    :feat add tips (#23)
    Co-authored-by: jinwentao <jinwentao@huya.com>

--- src/utils.js
+++ src/utils.js
@@ -87,3 +87,20 @@ export function onFullscreenChange(handler) {
     document.removeEventListener('MSFullscreenChange', handler);
   };
 }
+
+export function typeOf(obj) {
+  const toString = Object.prototype.toString;
+  const map = {
+    '[object Boolean]': 'boolean',
+    '[object Number]': 'number',
+    '[object String]': 'string',
+    '[object Function]': 'function',
+    '[object Array]': 'array',
+    '[object Date]': 'date',
+    '[object RegExp]': 'regExp',
+    '[object Undefined]': 'undefined',
+    '[object Null]': 'null',
+    '[object Object]': 'object'
+  };
+  return map[toString.call(obj)];
+}
