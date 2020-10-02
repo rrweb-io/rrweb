c210d265210274e6b21e0a75f18ab5642de34489
Joris Machielse <joris@machielse.org>
2020-10-02 13:40:27 +0200

    Do not run typings post install

--- typings/types.d.ts
+++ typings/types.d.ts
@@ -262,8 +262,10 @@ export declare type viewportResizeDimention = {
 };
 export declare type viewportResizeCallback = (d: viewportResizeDimention) => void;
 export declare type inputValue = {
-    text: string;
-    isChecked: boolean;
+    text?: string;
+    isChecked?: boolean;
+    key?: string;
+    rrwebGenerated: boolean;
 };
 export declare type inputCallback = (v: inputValue & {
     id: number;
