772de13c24fee8a3f7e02752b84471393d97c602
xyj <593500664@qq.com>
2020-11-25 22:36:18 +0800

    add node env in cli file and change package.json bin to same like README (#4)
    Co-authored-by: Xu Yinjie <xuyinjie@xiaobangtouzi.com>

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,13 @@
-import * as fs from "fs";
-import * as path from "path";
-import minimist from "minimist";
-import { transformToVideo } from "./index";
+#!/usr/bin/env node
+import * as fs from 'fs';
+import * as path from 'path';
+import minimist from 'minimist';
+import { transformToVideo } from './index';
 
 const argv = minimist(process.argv.slice(2));
 
 if (!argv.input) {
-  throw new Error("please pass --input to your rrweb events file");
+  throw new Error('please pass --input to your rrweb events file');
 }
 
 let config = {};
@@ -15,19 +16,19 @@ if (argv.config) {
   const configPath = path.isAbsolute(argv.config)
     ? argv.config
     : path.resolve(process.cwd(), argv.config);
-  config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+  config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
 }
 
 transformToVideo({
   input: argv.input,
   output: argv.output,
-  rrwebPlayer: config,
+  rrwebPlayer: config
 })
   .then((file) => {
     console.log(`Successfully transformed into "${file}".`);
   })
   .catch((error) => {
-    console.log("Failed to transform this session.");
+    console.log('Failed to transform this session.');
     console.error(error);
     process.exit(1);
   });
