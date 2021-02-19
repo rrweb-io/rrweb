aee3c1605c0e5e4ab056ff1dfc29e9e005e537e8
Justin Halsall <Juice10@users.noreply.github.com>
2021-02-19 12:50:22 +0100

    allow goto to specify if it should play or pause

--- src/Player.svelte
+++ src/Player.svelte
@@ -109,8 +109,8 @@
   export const pause = () => {
     controller.pause();
   };
-  export const goto = (timeOffset: number) => {
-    controller.goto(timeOffset);
+  export const goto = (timeOffset: number, play?: boolean) => {
+    controller.goto(timeOffset, play);
   };
 
   onMount(() => {
