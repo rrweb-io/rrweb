91b3593b5354a733b1b41962beb48c414950fb8f
Eoghan Murray <eoghan@getthere.ie>
2021-05-14 13:33:46 +0100

    When other events occur while a mouse move is being throttled, emit the mouse move event immediately so that there is negative timeOffsets never result in an out of sequence event stream

--- packages/rrweb/src/record/observer.ts
+++ packages/rrweb/src/record/observer.ts
@@ -55,6 +55,7 @@ import { callbackWrapper } from './error-handler';
 import dom, { mutationObserverCtor } from '@rrweb/utils';
 
 export const mutationBuffers: MutationBuffer[] = [];
+export let ongoingMove: ((now?: number) => void) | null = null;
 
 // Event.path is non-standard and used in some older browsers
 type NonStandardEvent = Omit<Event, 'composedPath'> & {
@@ -124,26 +125,31 @@ function initMoveObserver({
 
   let positions: mousePosition[] = [];
   let timeBaseline: number | null;
-  const wrappedCb = throttle(
-    callbackWrapper(
-      (
-        source:
-          | IncrementalSource.MouseMove
-          | IncrementalSource.TouchMove
-          | IncrementalSource.Drag,
-      ) => {
-        const totalOffset = Date.now() - timeBaseline!;
-        mousemoveCb(
-          positions.map((p) => {
-            p.timeOffset -= totalOffset;
-            return p;
-          }),
-          source,
-        );
-        positions = [];
-        timeBaseline = null;
-      },
-    ),
+  let source:
+    | IncrementalSource.MouseMove
+    | IncrementalSource.TouchMove
+    | IncrementalSource.Drag;
+
+  function moveEmission() {
+    if (!positions.length) {
+      // already emitted
+      return;
+    }
+    ongoingMove = null;
+    const totalOffset = Date.now() - timeBaseline!;
+    mousemoveCb(
+      positions.map((p) => {
+        p.timeOffset -= totalOffset;
+        return p;
+      }),
+      source,
+    );
+    positions = [];
+    timeBaseline = null;
+  }
+
+  const throttledMoveEmission = throttle(
+    callbackWrapper(moveEmission),
     callbackThreshold,
   );
   const updatePosition = callbackWrapper(
@@ -165,13 +171,14 @@ function initMoveObserver({
         });
         // it is possible DragEvent is undefined even on devices
         // that support event 'drag'
-        wrappedCb(
+        source =
           typeof DragEvent !== 'undefined' && evt instanceof DragEvent
             ? IncrementalSource.Drag
             : evt instanceof MouseEvent
             ? IncrementalSource.MouseMove
-            : IncrementalSource.TouchMove,
-        );
+            : IncrementalSource.TouchMove;
+        ongoingMove = moveEmission;
+        throttledMoveEmission();
       }),
       threshold,
       {
