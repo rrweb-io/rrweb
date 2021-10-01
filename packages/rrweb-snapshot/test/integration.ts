b0a2dd45dc50e33c12cdcb1114f9c14e5d44ee7d
Eoghan Murray <eoghan@getthere.ie>
2021-10-01 17:16:30 +0100

    Hygiene: clean up the xhtml namespace attribute; this is an artefact of the `serializeToString` method which we are using (I think) to be consistent with whitespace and to clean up invalid attributes. I'm removing as was confused as am adding tests related to doctypes

--- packages/rrweb-snapshot/test/integration.ts
+++ packages/rrweb-snapshot/test/integration.ts
@@ -111,7 +111,12 @@ describe('integration tests', function (this: ISuite) {
         await page.evaluate(`${this.code}
         const x = new XMLSerializer();
         const [snap] = rrweb.snapshot(document);
-        x.serializeToString(rrweb.rebuild(snap, { doc: document })[0]);
+        let out = x.serializeToString(rrweb.rebuild(snap, { doc: document })[0]);
+        if (document.querySelector('html').getAttribute('xmlns') !== 'http://www.w3.org/1999/xhtml') {
+          // this is just an artefact of serializeToString
+          out = out.replace(' xmlns=\"http://www.w3.org/1999/xhtml\"', '');
+        }
+        out;  // return
       `)
       ).replace(/\n\n/g, '');
       const result = matchSnapshot(rebuildHtml, __filename, title);
