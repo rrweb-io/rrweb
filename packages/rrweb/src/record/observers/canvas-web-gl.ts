6a614f67218c8b74012e2911736f28b502241829
Justin Halsall <Juice10@users.noreply.github.com>
2021-11-25 17:36:27 +0100

    Serialize WebGL variables

--- packages/rrweb/src/record/observers/canvas-web-gl.ts
+++ packages/rrweb/src/record/observers/canvas-web-gl.ts
@@ -9,6 +9,43 @@ import {
 } from '../../types';
 import { hookSetter, isBlocked, patch } from '../../utils';
 
+// from webgl-recorder: https://github.com/evanw/webgl-recorder/blob/bef0e65596e981ee382126587e2dcbe0fc7748e2/webgl-recorder.js#L50-L77
+const webGLVars: Record<string, Array<any>> = {};
+function getVariable(value: any) {
+  if (
+    value instanceof WebGLActiveInfo ||
+    value instanceof WebGLBuffer ||
+    value instanceof WebGLFramebuffer ||
+    value instanceof WebGLProgram ||
+    value instanceof WebGLRenderbuffer ||
+    value instanceof WebGLShader ||
+    value instanceof WebGLShaderPrecisionFormat ||
+    value instanceof WebGLTexture ||
+    value instanceof WebGLUniformLocation ||
+    value instanceof WebGLVertexArrayObject ||
+    // In Chrome, value won't be an instanceof WebGLVertexArrayObject.
+    (value && value.constructor.name == 'WebGLVertexArrayObjectOES') ||
+    typeof value === 'object'
+  ) {
+    const name = value.constructor.name;
+    const list = webGLVars[name] || (webGLVars[name] = []);
+    let index = list.indexOf(value);
+
+    if (index === -1) {
+      index = list.length;
+      list.push(value);
+    }
+
+    return `$${name}#${index}`;
+  }
+
+  return value;
+}
+
+const serializeArgs = (args: Array<any>) => {
+  return [...args].map(getVariable);
+};
+
 export default function initCanvasWebGLMutationObserver(
   cb: canvasMutationCallback,
   win: IWindow,
@@ -36,7 +73,7 @@ export default function initCanvasWebGLMutationObserver(
           ) {
             if (!isBlocked((this.canvas as unknown) as INode, blockClass)) {
               setTimeout(() => {
-                const recordArgs = [...args];
+                const recordArgs = serializeArgs([...args]);
                 cb({
                   id: mirror.getId((this.canvas as unknown) as INode),
                   type: CanvasContext.WebGL,
---
+++ packages/rrweb/test/record/webgl.test.ts
@@ -0,0 +1,175 @@
+/* tslint:disable no-console */
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as puppeteer from 'puppeteer';
+import {
+  recordOptions,
+  listenerHandler,
+  eventWithTime,
+  EventType,
+  IncrementalSource,
+  styleSheetRuleData,
+  CanvasContext,
+} from '../../src/types';
+import { assertSnapshot, launchPuppeteer } from '../utils';
+
+interface ISuite {
+  code: string;
+  browser: puppeteer.Browser;
+  page: puppeteer.Page;
+  events: eventWithTime[];
+}
+
+interface IWindow extends Window {
+  rrweb: {
+    record: (
+      options: recordOptions<eventWithTime>,
+    ) => listenerHandler | undefined;
+    addCustomEvent<T>(tag: string, payload: T): void;
+  };
+  emit: (e: eventWithTime) => undefined;
+}
+
+const setup = function (this: ISuite, content: string): ISuite {
+  const ctx = {} as ISuite;
+
+  beforeAll(async () => {
+    ctx.browser = await launchPuppeteer();
+
+    const bundlePath = path.resolve(__dirname, '../../dist/rrweb.min.js');
+    ctx.code = fs.readFileSync(bundlePath, 'utf8');
+  });
+
+  beforeEach(async () => {
+    ctx.page = await ctx.browser.newPage();
+    await ctx.page.goto('about:blank');
+    await ctx.page.setContent(content);
+    await ctx.page.evaluate(ctx.code);
+    ctx.events = [];
+    await ctx.page.exposeFunction('emit', (e: eventWithTime) => {
+      if (e.type === EventType.DomContentLoaded || e.type === EventType.Load) {
+        return;
+      }
+      ctx.events.push(e);
+    });
+
+    ctx.page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
+  });
+
+  afterEach(async () => {
+    await ctx.page.close();
+  });
+
+  afterAll(async () => {
+    await ctx.browser.close();
+  });
+
+  return ctx;
+};
+
+describe('record webgl', function (this: ISuite) {
+  jest.setTimeout(100_000);
+
+  const ctx: ISuite = setup.call(
+    this,
+    `
+      <!DOCTYPE html>
+      <html>
+        <body>
+          <canvas id="canvas"></canvas>
+        </body>
+      </html>
+    `,
+  );
+
+  it('will record changes to a canvas element', async () => {
+    await ctx.page.evaluate(() => {
+      const { record } = ((window as unknown) as IWindow).rrweb;
+      record({
+        recordCanvas: true,
+        emit(event: eventWithTime) {
+          ((window as unknown) as IWindow).emit(event);
+        },
+      });
+    });
+    await ctx.page.evaluate(() => {
+      var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+      var gl = canvas.getContext('webgl')!;
+
+      gl.clear(gl.COLOR_BUFFER_BIT);
+    });
+
+    await ctx.page.waitForTimeout(50);
+
+    const lastEvent = ctx.events[ctx.events.length - 1];
+    expect(lastEvent).toMatchObject({
+      data: {
+        source: IncrementalSource.CanvasMutation,
+        args: [16384],
+        type: CanvasContext.WebGL,
+        property: 'clear',
+      },
+    });
+  });
+
+  it('will record changes to a canvas element before the canvas gets added', async () => {
+    await ctx.page.evaluate(() => {
+      const { record } = ((window as unknown) as IWindow).rrweb;
+      record({
+        recordCanvas: true,
+        emit: ((window as unknown) as IWindow).emit,
+      });
+    });
+    await ctx.page.evaluate(() => {
+      var canvas = document.createElement('canvas');
+      var gl = canvas.getContext('webgl')!;
+      var program = gl.createProgram()!;
+      gl.linkProgram(program);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      document.body.appendChild(canvas);
+    });
+
+    await ctx.page.waitForTimeout(50);
+
+    const lastEvent = ctx.events[ctx.events.length - 1];
+    expect(lastEvent).toMatchObject({
+      data: {
+        source: IncrementalSource.CanvasMutation,
+        type: CanvasContext.WebGL,
+        property: 'clear',
+      },
+    });
+    // TODO: make this a jest snapshot
+  });
+
+  it('will record webgl variables', async () => {
+    await ctx.page.evaluate(() => {
+      const { record } = ((window as unknown) as IWindow).rrweb;
+      record({
+        recordCanvas: true,
+        emit: ((window as unknown) as IWindow).emit,
+      });
+    });
+    await ctx.page.evaluate(() => {
+      var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+      var gl = canvas.getContext('webgl')!;
+      var program0 = gl.createProgram()!;
+      gl.linkProgram(program0);
+      var program1 = gl.createProgram()!;
+      gl.linkProgram(program1);
+    });
+
+    await ctx.page.waitForTimeout(50);
+
+    const lastEvent = ctx.events[ctx.events.length - 1];
+    expect(lastEvent).toMatchObject({
+      data: {
+        source: IncrementalSource.CanvasMutation,
+        property: 'linkProgram',
+        type: CanvasContext.WebGL,
+        args: ['$WebGLProgram#1'], // `program1` is WebGLProgram, this is the second WebGLProgram variable (#1)
+      },
+    });
+  });
+});
