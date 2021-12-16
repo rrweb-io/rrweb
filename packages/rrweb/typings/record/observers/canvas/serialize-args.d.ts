d26676e3c96f1635f0d53b7ff21903a6e72f320f
Justin Halsall <Juice10@users.noreply.github.com>
2021-12-16 11:24:16 +0100

    Add types for serialize-args

--- packages/rrweb/typings/record/observers/canvas/serialize-args.d.ts
+++ packages/rrweb/typings/record/observers/canvas/serialize-args.d.ts
@@ -1,3 +1,4 @@
 import { SerializedWebGlArg } from '../../../types';
 export declare function serializeArg(value: any): SerializedWebGlArg;
 export declare const serializeArgs: (args: Array<any>) => SerializedWebGlArg[];
+export declare const saveWebGLVar: (value: any) => number | void;
