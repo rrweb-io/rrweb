9949db91ff75b236b546c08a40072911326863ec
Eoghan Murray <eoghan@getthere.ie>
2022-02-04 17:22:24 +0000

    Was experiencing an issue where the virtual parent optimization was destroying already applied styles during <head> mutations

--- packages/rrweb/src/replay/index.ts
+++ packages/rrweb/src/replay/index.ts
@@ -1401,11 +1401,13 @@ export class Replayer {
       /**
        * Why !isIframeINode(parent)? If parent element is an iframe, iframe document can't be appended to virtual parent.
        * Why !hasIframeChild? If we move iframe elements from dom to fragment document, we will lose the contentDocument of iframe. So we need to disable the virtual dom optimization if a parent node contains iframe elements.
+       * Why parent.tagName !== 'HEAD'? moving stylesheets out of the <head> element removes the styles from the document (and they don't get replaced at `parent = virtualParent`
        */
       if (
         useVirtualParent &&
         parentInDocument &&
         !isIframeINode(parent) &&
+        ((parent as unknown) as HTMLElement).tagName !== 'HEAD' &&
         !hasIframeChild
       ) {
         const virtualParent = (document.createDocumentFragment() as unknown) as INode;
