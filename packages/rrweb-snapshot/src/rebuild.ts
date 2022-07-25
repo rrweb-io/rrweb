5a9d6fafc14a3ff57c1133aaf43d2e237e2563c5
Yun Feng <yun.feng@anu.edu.au>
2022-07-25 18:16:29 +1000

    apply Juice10's suggestion

--- packages/rrweb-snapshot/src/rebuild.ts
+++ packages/rrweb-snapshot/src/rebuild.ts
@@ -228,13 +228,13 @@ function buildNode(
           // handle internal attributes
           if (tagName === 'canvas' && name === 'rr_dataURL') {
             const image = document.createElement('img');
-            image.src = value;
             image.onload = () => {
               const ctx = (node as HTMLCanvasElement).getContext('2d');
               if (ctx) {
                 ctx.drawImage(image, 0, 0, image.width, image.height);
               }
             };
+            image.src = value;
             type RRCanvasElement = {
               RRNodeType: NodeType;
               rr_dataURL: string;
