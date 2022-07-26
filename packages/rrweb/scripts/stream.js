5006ed4fd60e2252759ce1faabcad53527d33ff4
Justin Halsall <Juice10@users.noreply.github.com>
2022-07-26 16:22:01 +0200

    Add stream setup

---
+++ packages/rrweb/scripts/stream.js
@@ -0,0 +1,212 @@
+/* eslint:disable: no-console */
+
+import * as path from 'path';
+import * as fs from 'fs';
+import { EventEmitter } from 'node:events';
+import inquirer from 'inquirer';
+import puppeteer from 'puppeteer';
+import { fileURLToPath } from 'url';
+
+import { startServer, getServerURL } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const emitter = new EventEmitter();
+
+function getCode() {
+  const bundlePath = path.resolve(__dirname, '../dist/rrweb.js');
+  return fs.readFileSync(bundlePath, 'utf8');
+}
+
+async function startRecording(page, serverURL) {
+  await page.evaluate((serverURL) => {
+    const el = document.createElement('script');
+    el.addEventListener('load', () => {
+      const win = window;
+      win.__IS_RECORDING__ = true;
+      win.events = [];
+      win.rrweb?.record({
+        emit: (event) => {
+          win.events?.push(event);
+          win._replLog?.(event);
+        },
+        recordCanvas: true,
+        collectFonts: true,
+        inlineImages: true,
+      });
+    });
+    el.src = `${serverURL}/rrweb.js`;
+    document.head.appendChild(el);
+  }, serverURL);
+}
+
+function startReplay(page, serverURL) {
+  return page.evaluate((serverURL) => {
+    const el = document.createElement('script');
+    el.addEventListener('load', () => {
+      window.replayer = new rrweb.Replayer([], {
+        UNSAFE_replayCanvas: true,
+        liveMode: true,
+      });
+      window.replayer.startLive();
+    });
+    el.src = `${serverURL}/rrweb.js`;
+    document.head.appendChild(el);
+  }, serverURL);
+}
+
+async function resizeWindow(page, top, left, width, height) {
+  const session = await page.target().createCDPSession();
+  await page.setViewport({ height, width });
+  const { windowId } = await session.send('Browser.getWindowForTarget');
+  await session.send('Browser.setWindowBounds', {
+    bounds: { top, left, height, width },
+    windowId,
+  });
+}
+
+void (async () => {
+  const code = getCode();
+  const server = await startServer();
+  const serverURL = getServerURL(server);
+  let events = [];
+
+  await start();
+
+  const fakeGoto = async (page, url) => {
+    const intercept = (request) => {
+      void request.respond({
+        status: 200,
+        contentType: 'text/html',
+        body: ' ', // non-empty string or page will load indefinitely
+      });
+    };
+    await page.setRequestInterception(true);
+    page.on('request', intercept);
+    await page.goto(url);
+    await page.setRequestInterception(false);
+    page.off('request', intercept);
+  };
+
+  async function start() {
+    events = [];
+    const { url } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'url',
+        message:
+          'Enter the url you want to record, e.g https://react-redux.realworld.io: ',
+      },
+    ]);
+
+    await record(url);
+
+    const { shouldRecordAnother } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'shouldRecordAnother',
+        message: 'Record another one?',
+      },
+    ]);
+
+    emitter.emit('done');
+
+    if (shouldRecordAnother) {
+      await start();
+    } else {
+      process.exit();
+    }
+  }
+
+  async function record(url) {
+    if (url === '') url = 'https://react-redux.realworld.io';
+
+    const replayingBrowser = await puppeteer.launch({
+      headless: false,
+      // devtools: true,
+      defaultViewport: {
+        width: 1600,
+        height: 900,
+      },
+      args: [
+        '--start-maximized',
+        '--ignore-certificate-errors',
+        '--no-sandbox',
+      ],
+    });
+
+    const replayerPage = (await replayingBrowser.pages())[0];
+    await replayerPage.goto('about:blank');
+
+    await replayerPage.addStyleTag({
+      path: path.resolve(__dirname, '../dist/rrweb.min.css'),
+    });
+    await startReplay(replayerPage, serverURL);
+
+    const recordingBrowser = await puppeteer.launch({
+      headless: false,
+      // devtools: true,
+      defaultViewport: {
+        width: 1600,
+        height: 900,
+      },
+      args: [
+        '--start-maximized',
+        '--ignore-certificate-errors',
+        '--no-sandbox',
+      ],
+    });
+
+    const recordedPage = (await recordingBrowser.pages())[0];
+    if (!recordedPage) {
+      throw new Error('No recorded page found');
+    }
+    // disables content security policy which enables us to insert rrweb as a script tag
+    await recordedPage.setBypassCSP(true);
+
+    await Promise.all([
+      resizeWindow(recordedPage, 0, 0, 800, 800),
+      resizeWindow(replayerPage, 0, 800, 800, 800),
+    ]);
+
+    await recordedPage.goto(url, {
+      waitUntil: 'domcontentloaded',
+      timeout: 300000,
+    });
+
+    if (!replayerPage) throw new Error('No replayer page found');
+
+    await recordedPage.exposeFunction('_replLog', (event) => {
+      replayerPage.evaluate((event) => window.replayer?.addEvent(event), event);
+    });
+    await startRecording(recordedPage, serverURL);
+    recordedPage.on('framenavigated', async () => {
+      const isRecording = await recordedPage.evaluate(
+        'window.__IS_RECORDING__',
+      );
+      if (!isRecording) {
+        await startRecording(recordedPage, serverURL);
+      }
+    });
+
+    emitter.once('done', async () => {
+      const pages = [
+        ...(await recordingBrowser.pages()),
+        ...(await replayingBrowser.pages()),
+      ];
+      await server.close();
+      await Promise.all(pages.map((page) => page.close()));
+      await recordingBrowser.close();
+      await replayingBrowser.close();
+    });
+  }
+
+  process
+    .on('uncaughtException', (error) => {
+      console.error(error);
+    })
+    .on('unhandledRejection', (error) => {
+      console.error(error);
+    });
+})();
---
+++ packages/rrweb/scripts/utils.js
@@ -0,0 +1,59 @@
+import * as path from 'path';
+import * as http from 'http';
+import * as url from 'url';
+import * as fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const startServer = (defaultPort = 3030) =>
+  new Promise((resolve) => {
+    const mimeType = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+    };
+    const s = http.createServer((req, res) => {
+      const parsedUrl = url.parse(req.url);
+      const sanitizePath = path
+        .normalize(parsedUrl.pathname)
+        .replace(/^(\.\.[\/\\])+/, '');
+      let pathname = path.join(__dirname, sanitizePath);
+      if (/^\/rrweb.*\.js.*/.test(sanitizePath)) {
+        pathname = path.join(__dirname, `../dist`, sanitizePath);
+      }
+      try {
+        const data = fs.readFileSync(pathname);
+        const ext = path.parse(pathname).ext;
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-type');
+        setTimeout(() => {
+          res.end(data);
+        }, 100);
+      } catch (error) {
+        res.end();
+      }
+    });
+    s.listen(defaultPort)
+      .on('listening', () => {
+        resolve(s);
+      })
+      .on('error', (e) => {
+        console.log('port in use, trying next one');
+        s.listen().on('listening', () => {
+          resolve(s);
+        });
+      });
+  });
+
+export function getServerURL(server) {
+  const address = server.address();
+  if (address && typeof address !== 'string') {
+    return `http://localhost:${address.port}`;
+  } else {
+    return `${address}`;
+  }
+}
