7e633e5b7f07ba782713552f4631aa582bc1c18e
Yun Feng <yun.feng@anu.edu.au>
2022-09-17 09:34:37 +0800

    Update packages/rrweb/src/record/constructableStyleSheets.d.ts
    Co-authored-by: Justin Halsall <Juice10@users.noreply.github.com>

--- packages/rrweb/src/record/constructableStyleSheets.d.ts
+++ packages/rrweb/src/record/constructableStyleSheets.d.ts
@@ -1,7 +1,7 @@
 // This informs the TS compiler about constructed stylesheets.
 // It can be removed when this is fixed: https://github.com/Microsoft/TypeScript/issues/30022
 declare interface DocumentOrShadowRoot {
-  adoptedStyleSheets: CSSStyleSheet[];
+  adoptedStyleSheets?: CSSStyleSheet[];
 }
 
 declare interface CSSStyleSheet {
