ea28b1b2a872fb293bf2525b5bd81790757a3778
Justin Halsall <Juice10@users.noreply.github.com>
2022-09-20 12:09:01 +0200

    Make maximum scale configurable

--- packages/rrweb-player/src/Player.svelte
+++ packages/rrweb-player/src/Player.svelte
@@ -14,6 +14,7 @@
 
   export let width = 1024;
   export let height = 576;
+  export let maxScale = 1;
   export let events: eventWithTime[] = [];
   export let skipInactive = true;
   export let autoPlay = true;
@@ -55,8 +56,10 @@
   ) => {
     const widthScale = width / frameDimension.width;
     const heightScale = height / frameDimension.height;
+    const scale = [widthScale, heightScale];
+    if (maxScale) scale.push(maxScale);
     el.style.transform =
-      `scale(${Math.min(widthScale, heightScale, 1)})` +
+      `scale(${Math.min(...scale)})` +
       'translate(-50%, -50%)';
   };
 
