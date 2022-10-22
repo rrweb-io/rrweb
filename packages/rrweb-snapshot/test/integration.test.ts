4c2f310cefe96f761079a8c13deb42dcad7b7d73
Justin Halsall <Juice10@users.noreply.github.com>
2022-10-22 23:02:31 +0200

    Migrate rrweb-snapshot to vite

--- packages/rrweb-snapshot/test/integration.test.ts
+++ packages/rrweb-snapshot/test/integration.test.ts
@@ -3,12 +3,8 @@ import * as path from 'path';
 import * as http from 'http';
 import * as url from 'url';
 import * as puppeteer from 'puppeteer';
-import * as rollup from 'rollup';
-import * as typescript from 'rollup-plugin-typescript2';
 import * as assert from 'assert';
 import { waitForRAF } from './utils';
-
-const _typescript = (typescript as unknown) as () => rollup.Plugin;
 
 const htmlFolder = path.join(__dirname, 'html');
 const htmls = fs.readdirSync(htmlFolder).map((filePath) => {
@@ -23,7 +19,7 @@ interface IMimeType {
   [key: string]: string;
 }
 
-const startServer = () =>
+const startServer = (defaultPort: number = 3030) =>
   new Promise<http.Server>((resolve) => {
     const mimeType: IMimeType = {
       '.html': 'text/html',
@@ -49,13 +45,37 @@ const startServer = () =>
         res.end();
       }
     });
-    s.listen(3030).on('listening', () => {
-      resolve(s);
-    });
+    s.listen(defaultPort)
+      .on('listening', () => {
+        resolve(s);
+      })
+      .on('error', (e) => {
+        s.listen().on('listening', () => {
+          resolve(s);
+        });
+      });
   });
+
+function getServerURL(server: http.Server): string {
+  const address = server.address();
+  if (address && typeof address !== 'string') {
+    return `http://localhost:${address.port}`;
+  } else {
+    return `${address}`;
+  }
+}
+
+function sanitizeSnapshot(snapshot: string): string {
+  return snapshot.replace(/localhost:[0-9]+/g, 'localhost:3030');
+}
+
+function assertSnapshot(snapshot: string): void {
+  expect(sanitizeSnapshot(snapshot)).toMatchSnapshot();
+}
 
 interface ISuite {
   server: http.Server;
+  serverURL: string;
   browser: puppeteer.Browser;
   code: string;
 }
@@ -63,26 +83,21 @@ interface ISuite {
 describe('integration tests', function (this: ISuite) {
   jest.setTimeout(30_000);
   let server: ISuite['server'];
+  let serverURL: ISuite['serverURL'];
   let browser: ISuite['browser'];
   let code: ISuite['code'];
 
   beforeAll(async () => {
     server = await startServer();
+    serverURL = getServerURL(server);
     browser = await puppeteer.launch({
       // headless: false,
     });
 
-    const bundle = await rollup.rollup({
-      input: path.resolve(__dirname, '../src/index.ts'),
-      plugins: [_typescript()],
-    });
-    const {
-      output: [{ code: _code }],
-    } = await bundle.generate({
-      name: 'rrweb',
-      format: 'iife',
-    });
-    code = _code;
+    code = fs.readFileSync(
+      path.resolve(__dirname, '../dist/rrweb-snapshot.umd.cjs'),
+      'utf-8',
+    );
   });
 
   afterAll(async () => {
@@ -102,7 +117,7 @@ describe('integration tests', function (this: ISuite) {
       if (html.filePath === 'iframe.html') {
         // loading directly is needed to ensure we don't trigger compatMode='BackCompat'
         // which happens before setContent can be called
-        await page.goto(`http://localhost:3030/html/${html.filePath}`, {
+        await page.goto(`${serverURL}/html/${html.filePath}`, {
           waitUntil: 'load',
         });
         const outerCompatMode = await page.evaluate('document.compatMode');
@@ -123,7 +138,7 @@ describe('integration tests', function (this: ISuite) {
         );
       } else {
         // loading indirectly is improtant for relative path testing
-        await page.goto(`http://localhost:3030/html`);
+        await page.goto(`${serverURL}/html`);
         await page.setContent(html.src, {
           waitUntil: 'load',
         });
@@ -131,8 +146,8 @@ describe('integration tests', function (this: ISuite) {
       const rebuildHtml = (
         await page.evaluate(`${code}
         const x = new XMLSerializer();
-        const snap = rrweb.snapshot(document);
-        let out = x.serializeToString(rrweb.rebuild(snap, { doc: document }));
+        const snap = rrwebSnapshot.snapshot(document);
+        let out = x.serializeToString(rrwebSnapshot.rebuild(snap, { doc: document }));
         if (document.querySelector('html').getAttribute('xmlns') !== 'http://www.w3.org/1999/xhtml') {
           // this is just an artefact of serializeToString
           out = out.replace(' xmlns=\"http://www.w3.org/1999/xhtml\"', '');
@@ -140,7 +155,7 @@ describe('integration tests', function (this: ISuite) {
         out;  // return
       `)
       ).replace(/\n\n/g, '');
-      expect(rebuildHtml).toMatchSnapshot();
+      assertSnapshot(rebuildHtml);
     });
   }
 
@@ -149,7 +164,7 @@ describe('integration tests', function (this: ISuite) {
     // console for debug
     page.on('console', (msg) => console.log(msg.text()));
 
-    await page.goto('http://localhost:3030/html/compat-mode.html', {
+    await page.goto(`${serverURL}/html/compat-mode.html`, {
       waitUntil: 'load',
     });
     const compatMode = await page.evaluate('document.compatMode');
@@ -167,14 +182,14 @@ describe('integration tests', function (this: ISuite) {
       `pre-check: images will be rendered ~326px high in BackCompat mode, and ~588px in CSS1Compat mode; getting: ${renderedHeight}px`,
     );
     const rebuildRenderedHeight = await page.evaluate(`${code}
-const snap = rrweb.snapshot(document);
+const snap = rrwebSnapshot.snapshot(document);
 const iframe = document.createElement('iframe');
 iframe.setAttribute('width', document.body.clientWidth)
 iframe.setAttribute('height', document.body.clientHeight)
 iframe.style.transform = 'scale(0.3)'; // mini-me
 document.body.appendChild(iframe);
 // magic here! rebuild in a new iframe
-const rebuildNode = rrweb.rebuild(snap, { doc: iframe.contentDocument })[0];
+const rebuildNode = rrwebSnapshot.rebuild(snap, { doc: iframe.contentDocument })[0];
 iframe.contentDocument.querySelector('center').clientHeight
 `);
     const rebuildCompatMode = await page.evaluate(
@@ -194,11 +209,11 @@ iframe.contentDocument.querySelector('center').clientHeight
   it('correctly saves images offline', async () => {
     const page: puppeteer.Page = await browser.newPage();
 
-    await page.goto('http://localhost:3030/html/picture.html', {
+    await page.goto(`${serverURL}/html/picture.html`, {
       waitUntil: 'load',
     });
     await page.waitForSelector('img', { timeout: 1000 });
-    await page.evaluate(`${code}var snapshot = rrweb.snapshot(document, {
+    await page.evaluate(`${code}var snapshot = rrwebSnapshot.snapshot(document, {
         dataURLOptions: { type: "image/webp", quality: 0.8 },
         inlineImages: true,
         inlineStylesheet: false
@@ -212,11 +227,11 @@ iframe.contentDocument.querySelector('center').clientHeight
   it('correctly saves blob:images offline', async () => {
     const page: puppeteer.Page = await browser.newPage();
 
-    await page.goto('http://localhost:3030/html/picture-blob.html', {
+    await page.goto(`${serverURL}/html/picture-blob.html`, {
       waitUntil: 'load',
     });
     await page.waitForSelector('img', { timeout: 1000 });
-    await page.evaluate(`${code}var snapshot = rrweb.snapshot(document, {
+    await page.evaluate(`${code}var snapshot = rrwebSnapshot.snapshot(document, {
         dataURLOptions: { type: "image/webp", quality: 0.8 },
         inlineImages: true,
         inlineStylesheet: false
@@ -230,13 +245,13 @@ iframe.contentDocument.querySelector('center').clientHeight
   it('correctly saves images in iframes offline', async () => {
     const page: puppeteer.Page = await browser.newPage();
 
-    await page.goto('http://localhost:3030/html/picture-in-frame.html', {
+    await page.goto(`${serverURL}/html/picture-in-frame.html`, {
       waitUntil: 'load',
     });
     await page.waitForSelector('iframe', { timeout: 1000 });
     await waitForRAF(page); // wait for page to render
     await page.evaluate(`${code}
-        rrweb.snapshot(document, {
+        rrwebSnapshot.snapshot(document, {
         dataURLOptions: { type: "image/webp", quality: 0.8 },
         inlineImages: true,
         inlineStylesheet: false,
@@ -255,13 +270,13 @@ iframe.contentDocument.querySelector('center').clientHeight
   it('correctly saves blob:images in iframes offline', async () => {
     const page: puppeteer.Page = await browser.newPage();
 
-    await page.goto('http://localhost:3030/html/picture-blob-in-frame.html', {
+    await page.goto(`${serverURL}/html/picture-blob-in-frame.html`, {
       waitUntil: 'load',
     });
     await page.waitForSelector('iframe', { timeout: 1000 });
     await waitForRAF(page); // wait for page to render
     await page.evaluate(`${code}
-        rrweb.snapshot(document, {
+        rrwebSnapshot.snapshot(document, {
         dataURLOptions: { type: "image/webp", quality: 0.8 },
         inlineImages: true,
         inlineStylesheet: false,
@@ -281,26 +296,21 @@ iframe.contentDocument.querySelector('center').clientHeight
 describe('iframe integration tests', function (this: ISuite) {
   jest.setTimeout(30_000);
   let server: ISuite['server'];
+  let serverURL: ISuite['serverURL'];
   let browser: ISuite['browser'];
   let code: ISuite['code'];
 
   beforeAll(async () => {
     server = await startServer();
+    serverURL = getServerURL(server);
     browser = await puppeteer.launch({
       // headless: false,
     });
 
-    const bundle = await rollup.rollup({
-      input: path.resolve(__dirname, '../src/index.ts'),
-      plugins: [_typescript()],
-    });
-    const {
-      output: [{ code: _code }],
-    } = await bundle.generate({
-      name: 'rrweb',
-      format: 'iife',
-    });
-    code = _code;
+    code = fs.readFileSync(
+      path.resolve(__dirname, '../dist/rrweb-snapshot.umd.cjs'),
+      'utf-8',
+    );
   });
 
   afterAll(async () => {
@@ -312,43 +322,38 @@ describe('iframe integration tests', function (this: ISuite) {
     const page: puppeteer.Page = await browser.newPage();
     // console for debug
     page.on('console', (msg) => console.log(msg.text()));
-    await page.goto(`http://localhost:3030/iframe-html/main.html`, {
+    await page.goto(`${serverURL}/iframe-html/main.html`, {
       waitUntil: 'load',
     });
     const snapshotResult = JSON.stringify(
       await page.evaluate(`${code};
-      rrweb.snapshot(document);
+      rrwebSnapshot.snapshot(document);
     `),
       null,
       2,
     );
-    expect(snapshotResult).toMatchSnapshot();
+    assertSnapshot(snapshotResult);
   });
 });
 
 describe('shadow DOM integration tests', function (this: ISuite) {
   jest.setTimeout(30_000);
   let server: ISuite['server'];
+  let serverURL: ISuite['serverURL'];
   let browser: ISuite['browser'];
   let code: ISuite['code'];
 
   beforeAll(async () => {
     server = await startServer();
+    serverURL = getServerURL(server);
     browser = await puppeteer.launch({
       // headless: false,
     });
 
-    const bundle = await rollup.rollup({
-      input: path.resolve(__dirname, '../src/index.ts'),
-      plugins: [_typescript()],
-    });
-    const {
-      output: [{ code: _code }],
-    } = await bundle.generate({
-      name: 'rrweb',
-      format: 'iife',
-    });
-    code = _code;
+    code = fs.readFileSync(
+      path.resolve(__dirname, '../dist/rrweb-snapshot.umd.cjs'),
+      'utf-8',
+    );
   });
 
   afterAll(async () => {
@@ -360,16 +365,16 @@ describe('shadow DOM integration tests', function (this: ISuite) {
     const page: puppeteer.Page = await browser.newPage();
     // console for debug
     page.on('console', (msg) => console.log(msg.text()));
-    await page.goto(`http://localhost:3030/html/shadow-dom.html`, {
+    await page.goto(`${serverURL}/html/shadow-dom.html`, {
       waitUntil: 'load',
     });
     const snapshotResult = JSON.stringify(
       await page.evaluate(`${code};
-      rrweb.snapshot(document);
+      rrwebSnapshot.snapshot(document);
     `),
       null,
       2,
     );
-    expect(snapshotResult).toMatchSnapshot();
+    assertSnapshot(snapshotResult);
   });
 });
---
+++ packages/rrweb-snapshot/vite.config.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import dts from 'vite-plugin-dts';
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  build: {
+    // See https://vitejs.dev/guide/build.html#library-mode
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'rrwebSnapshot',
+    },
+
+    // Leaving this unminified so you can see what exactly gets included in
+    // the bundles
+    minify: false,
+
+    sourcemap: true,
+  },
+  plugins: [dts()],
+};
