d57d7809bde4adb0e5d5168ec32e625003e17b2f
Justin Halsall <Juice10@users.noreply.github.com>
2022-10-22 23:24:37 +0200

    Unify configs

---
+++ vite.config.default.ts
@@ -0,0 +1,20 @@
+import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite';
+export default function (entry: string, name: string) {
+  return defineConfig({
+    build: {
+      // See https://vitejs.dev/guide/build.html#library-mode
+      lib: {
+        entry,
+        name,
+      },
+
+      // Leaving this unminified so you can see what exactly gets included in
+      // the bundles
+      minify: false,
+
+      sourcemap: true,
+    },
+    plugins: [dts()],
+  });
+}
