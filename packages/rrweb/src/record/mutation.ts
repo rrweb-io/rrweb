f1c8d0b66345b2d82ebc252db5c8886cba21eb61
Justin Halsall <Juice10@users.noreply.github.com>
2022-10-25 15:19:31 +0200

    this.mirror was dropped on attachIframe

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -325,7 +325,7 @@ export default class MutationBuffer {
           }
         },
         onIframeLoad: (iframe, childSn) => {
-          this.iframeManager.attachIframe(iframe, childSn, this.mirror);
+          this.iframeManager.attachIframe(iframe, childSn);
           this.shadowDomManager.observeAttachShadow(iframe);
         },
         onStylesheetLoad: (link, childSn) => {
