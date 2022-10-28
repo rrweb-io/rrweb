22fdfaf68e28512e07e3ea92c2634a4415bda411
luocq3 <luocq3@asiainfo.com>
2022-10-28 11:51:37 +0800

    shadow root use the correct dom in iframe

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -321,7 +321,7 @@ export default class MutationBuffer {
             );
           }
           if (hasShadowRoot(n)) {
-            this.shadowDomManager.addShadowRoot(n.shadowRoot, document);
+            this.shadowDomManager.addShadowRoot(n.shadowRoot, this.doc);
           }
         },
         onIframeLoad: (iframe, childSn) => {
