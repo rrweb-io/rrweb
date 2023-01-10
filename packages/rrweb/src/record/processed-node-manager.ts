03ed8da84e07fe9f863c1cbd196f290df7f4f603
Justin Halsall <Juice10@users.noreply.github.com>
2023-01-10 11:31:54 +0100

    Remove unused nodeSet

--- packages/rrweb/src/record/processed-node-manager.ts
+++ packages/rrweb/src/record/processed-node-manager.ts
@@ -4,7 +4,6 @@ import type MutationBuffer from './mutation';
  * Keeps a log of nodes that could show up in multiple mutation buffer but shouldn't be handled twice.
  */
 export default class ProcessedNodeManager {
-  private nodeSet: WeakSet<Node> = new WeakSet();
   private nodeMap: WeakMap<Node, Set<MutationBuffer>> = new WeakMap();
 
   constructor() {
@@ -18,10 +17,6 @@ export default class ProcessedNodeManager {
     });
   }
 
-  public has(node: Node) {
-    return this.nodeSet.has(node);
-  }
-
   public inOtherBuffer(node: Node, thisBuffer: MutationBuffer) {
     const buffers = this.nodeMap.get(node);
     return (
@@ -30,11 +25,10 @@ export default class ProcessedNodeManager {
   }
 
   public add(node: Node, buffer: MutationBuffer) {
-    this.nodeSet.add(node);
     this.nodeMap.set(node, (this.nodeMap.get(node) || new Set()).add(buffer));
   }
 
   private clear() {
-    this.nodeSet = new WeakSet();
+    this.nodeMap = new WeakMap();
   }
 }
