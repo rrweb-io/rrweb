c748b2f5673e31aba742c174eee98f8bac3ebf09
Yun Feng <yun.feng@smartx.com>
2023-01-17 20:44:34 +1100

    add checker for replayer to make it stable when data has duplicated nodes

--- packages/rrweb-snapshot/src/utils.ts
+++ packages/rrweb-snapshot/src/utils.ts
@@ -5,6 +5,12 @@ import {
   nodeMetaMap,
   IMirror,
   serializedNodeWithId,
+  serializedNode,
+  NodeType,
+  documentNode,
+  documentTypeNode,
+  textNode,
+  elementNode,
 } from './types';
 
 export function isElement(n: Node): n is Element {
@@ -212,4 +218,31 @@ export function is2DCanvasBlank(canvas: HTMLCanvasElement): boolean {
     }
   }
   return true;
+}
+
+export function isNodeMetaEqual(a: serializedNode, b: serializedNode): boolean {
+  if (!a || !b || a.type !== b.type) return false;
+  if (a.type === NodeType.Document)
+    return a.compatMode === (b as documentNode).compatMode;
+  else if (a.type === NodeType.DocumentType)
+    return (
+      a.name === (b as documentTypeNode).name &&
+      a.publicId === (b as documentTypeNode).publicId &&
+      a.systemId === (b as documentTypeNode).systemId
+    );
+  else if (
+    a.type === NodeType.Comment ||
+    a.type === NodeType.Text ||
+    a.type === NodeType.CDATA
+  )
+    return a.textContent === (b as textNode).textContent;
+  else if (a.type === NodeType.Element)
+    return (
+      a.tagName === (b as elementNode).tagName &&
+      JSON.stringify(a.attributes) ===
+        JSON.stringify((b as elementNode).attributes) &&
+      a.isSVG === (b as elementNode).isSVG &&
+      a.needBlock === (b as elementNode).needBlock
+    );
+  return false;
 }
