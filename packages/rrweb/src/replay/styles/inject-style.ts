fdc837b8ff35e4e2d69120e6a2c394592af227cc
alexfullview <alex@fullview.io>
2023-03-10 13:46:05 +0200

    format inject

--- packages/rrweb/src/replay/styles/inject-style.ts
+++ packages/rrweb/src/replay/styles/inject-style.ts
@@ -1,6 +1,6 @@
 const rules: (blockClass: string) => string[] = (blockClass: string) => [
-    `.${blockClass} { background: currentColor }`,
-    'noscript { display: none !important; }',
-  ];
-  
-  export default rules;
+  `.${blockClass} { background: currentColor }`,
+  'noscript { display: none !important; }',
+];
+
+export default rules;
