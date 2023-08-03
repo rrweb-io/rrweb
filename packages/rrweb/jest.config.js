d4e1ef0b1f3bd49bada0c1d27c6e0f0db5314396
Justin Halsall <Juice10@users.noreply.github.com>
2023-08-03 10:46:46 +0200

    Document snapshotFormat jest config

--- packages/rrweb/jest.config.js
+++ packages/rrweb/jest.config.js
@@ -6,6 +6,11 @@ export default {
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
   },
+  /**
+   * Keeps old (pre-jest 29) snapshot format
+   * its a bit ugly and harder to read than the new format,
+   * so we might want to remove this in its own PR
+   */
   snapshotFormat: {
     escapeString: true,
     printBasicPrototype: true,
