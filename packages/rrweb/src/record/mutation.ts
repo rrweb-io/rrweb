4d540efa000bc2133aa89c5878ccd1678bf229c0
Ben White <ben@benjackwhite.co.uk>
2023-10-13 12:35:45 +0200

    Update packages/rrweb/src/record/mutation.ts
    Co-authored-by: Eoghan Murray <eoghan@getthere.ie>

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -522,7 +522,7 @@ export default class MutationBuffer {
                 this.maskTextSelector,
               ) && value
                 ? this.maskTextFn
-                  ? this.maskTextFn(value, getElementFromNode(m.target))
+                  ? this.maskTextFn(value, getClosestElementForNode(m.target))
                   : value.replace(/[\S]/g, '*')
                 : value,
             node: m.target,
