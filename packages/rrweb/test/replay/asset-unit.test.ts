62655e4b1a5ea0daa0363b6b7362b8a4c8d2149d
Justin Halsall <Juice10@users.noreply.github.com>
2023-11-24 17:38:29 +0100

    Add replay for cached asset on attribute changes

--- packages/rrweb/test/replay/asset-unit.test.ts
+++ packages/rrweb/test/replay/asset-unit.test.ts
@@ -101,7 +101,7 @@ describe('AssetManager', () => {
   });
 
   it('should execute hook when an asset is added', async () => {
-    jest.useFakeTimers();
+    vi.useFakeTimers();
     const url = 'https://example.com/image.png';
     const event: assetEvent = {
       type: EventType.Asset,
@@ -113,9 +113,9 @@ describe('AssetManager', () => {
     void assetManager.add(event);
     const promise = assetManager.whenReady(url);
 
-    jest.spyOn(URL, 'createObjectURL').mockReturnValue('objectURL');
+    vi.spyOn(URL, 'createObjectURL').mockReturnValue('objectURL');
 
-    jest.runAllTimers();
+    vi.runAllTimers();
 
     await expect(promise).resolves.toEqual({
       status: 'loaded',
@@ -124,7 +124,7 @@ describe('AssetManager', () => {
   });
 
   it('should send status reset to callbacks when reset', async () => {
-    jest.useFakeTimers();
+    vi.useFakeTimers();
     const url = 'https://example.com/image.png';
     const event: assetEvent = {
       type: EventType.Asset,
@@ -137,8 +137,98 @@ describe('AssetManager', () => {
     const promise = assetManager.whenReady(url);
 
     assetManager.reset();
-    jest.runAllTimers();
+    vi.runAllTimers();
 
     await expect(promise).resolves.toEqual({ status: 'reset' });
+  });
+
+  const validCombinations = [
+    ['img', ['src', 'srcset']],
+    ['video', ['src']],
+    ['audio', ['src']],
+    ['embed', ['src']],
+    ['source', ['src']],
+    ['track', ['src']],
+    ['input', ['src']],
+    ['iframe', ['src']],
+    ['object', ['src']],
+  ] as const;
+
+  const invalidCombinations = [
+    ['img', ['href']],
+    ['script', ['href']],
+    ['link', ['src']],
+    ['video', ['href']],
+    ['audio', ['href']],
+    ['div', ['src']],
+    ['source', ['href']],
+    ['track', ['href']],
+    ['input', ['href']],
+    ['iframe', ['href']],
+    ['object', ['href']],
+  ] as const;
+
+  validCombinations.forEach(([tagName, attributes]) => {
+    const element = document.createElement(tagName);
+    attributes.forEach((attribute) => {
+      it(`should correctly identify <${tagName} ${attribute}> as cacheable`, () => {
+        expect(assetManager.isAttributeCacheable(element, attribute)).toBe(
+          true,
+        );
+      });
+    });
+  });
+
+  invalidCombinations.forEach(([tagName, attributes]) => {
+    const element = document.createElement(tagName);
+    attributes.forEach((attribute) => {
+      it(`should correctly identify <${tagName} ${attribute}> as NOT cacheable`, () => {
+        expect(assetManager.isAttributeCacheable(element, attribute)).toBe(
+          false,
+        );
+      });
+    });
+  });
+
+  it("should be able to modify a node's attribute once asset is loaded", async () => {
+    const url = 'https://example.com/image.png';
+    const event: assetEvent = {
+      type: EventType.Asset,
+      data: {
+        url,
+        payload: examplePayload,
+      },
+    };
+    vi.spyOn(URL, 'createObjectURL').mockReturnValue('objectURL');
+
+    const element = document.createElement('img');
+    element.setAttribute('src', url);
+
+    const promise = assetManager.manageAttribute(element, 'src');
+
+    await assetManager.add(event);
+    await promise;
+
+    expect(element.getAttribute('src')).toBe('objectURL');
+  });
+
+  it("should be able to modify a node's attribute for previously loaded assets", async () => {
+    const url = 'https://example.com/image.png';
+    const event: assetEvent = {
+      type: EventType.Asset,
+      data: {
+        url,
+        payload: examplePayload,
+      },
+    };
+    vi.spyOn(URL, 'createObjectURL').mockReturnValue('objectURL');
+    await assetManager.add(event);
+
+    const element = document.createElement('img');
+    element.setAttribute('src', url);
+
+    await assetManager.manageAttribute(element, 'src');
+
+    expect(element.getAttribute('src')).toBe('objectURL');
   });
 });
