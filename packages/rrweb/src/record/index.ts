c8c905e6ce4cb6dae3785d0b93997d35c095c114
Justin Halsall <Juice10@users.noreply.github.com>
2023-11-27 17:26:07 +0100

    Add asset capture config to meta events

--- packages/rrweb/src/record/index.ts
+++ packages/rrweb/src/record/index.ts
@@ -384,6 +384,7 @@ function record<T = eventWithTime>(
           href: window.location.href,
           width: getWindowWidth(),
           height: getWindowHeight(),
+          assetCapture,
         },
       },
       isCheckout,
