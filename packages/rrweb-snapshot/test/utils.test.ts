07fd05a24a8b0279d907cad156234764a7696a72
Justin Halsall <Juice10@users.noreply.github.com>
2023-11-27 23:36:42 +0100

    NodeType was moved to @rrweb/types

--- packages/rrweb-snapshot/test/utils.test.ts
+++ packages/rrweb-snapshot/test/utils.test.ts
@@ -2,6 +2,12 @@
  * @vitest-environment jsdom
  */
 import { describe, it, test, expect } from 'vitest';
+<<<<<<< HEAD
+||||||| parent of a5097a7cb (NodeType was moved to @rrweb/types)
+import { NodeType, serializedNode } from '@rrweb/types';
+=======
+import { NodeType } from '@rrweb/types';
+>>>>>>> a5097a7cb (NodeType was moved to @rrweb/types)
 import {
   escapeImportStatement,
   extractFileExtension,
@@ -9,8 +15,14 @@ import {
   isNodeMetaEqual,
   stringifyStylesheet,
 } from '../src/utils';
+<<<<<<< HEAD
 import { NodeType } from '@rrweb/types';
 import type { serializedNode, serializedNodeWithId } from '@rrweb/types';
+||||||| parent of a5097a7cb (NodeType was moved to @rrweb/types)
+import type { serializedNodeWithId } from '@rrweb/types';
+=======
+import type { serializedNode, serializedNodeWithId } from '@rrweb/types';
+>>>>>>> a5097a7cb (NodeType was moved to @rrweb/types)
 
 describe('utils', () => {
   describe('isNodeMetaEqual()', () => {
