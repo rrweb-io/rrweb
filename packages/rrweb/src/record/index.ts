99d2b21a348628082717b6b5c5c2dc20dcd6f72d
Justin Halsall <Juice10@users.noreply.github.com>
2023-11-27 17:07:02 +0100

    Move assetCapture params to @rrweb/types and add them to meta events

--- packages/rrweb/src/record/index.ts
+++ packages/rrweb/src/record/index.ts
@@ -84,8 +84,8 @@ function record<T = eventWithTime>(
     collectFonts = false,
     inlineImages = false,
     assetCapture = {
-      captureObjectURLs: true,
-      captureOrigins: false,
+      objectURLs: true,
+      origins: false,
     },
     plugins,
     keepIframeSrcFn = () => false,
