693b5b036217b5f8fbfc5dadd7cd79261acd5a08
Justin Halsall <Juice10@users.noreply.github.com>
2023-11-28 21:05:22 +0100

    Fix rrdom build

--- packages/rrdom/jest.config.js
+++ packages/rrdom/jest.config.js
@@ -2,4 +2,14 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['./jest.setup.ts'],
+  /**
+   * Keeps old (pre-jest 29) snapshot format
+   * its a bit ugly and harder to read than the new format,
+   * so we might want to remove this in its own PR
+   */
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
 };
