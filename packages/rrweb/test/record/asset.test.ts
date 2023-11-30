ffa46d5031b206d4b83d90dbbb8a6be41ca06e71
Justin Halsall <Juice10@users.noreply.github.com>
2023-11-30 16:43:42 +0100

    Test: Add captureAssets config to meta event

--- packages/rrweb/test/record/asset.test.ts
+++ packages/rrweb/test/record/asset.test.ts
@@ -478,6 +478,30 @@ describe('asset caching', function (this: ISuite) {
       },
     );
 
+    it('adds captureAssets config to meta event', async () => {
+      await ctx.page.waitForNetworkIdle({ idleTime: 100 });
+      await waitForRAF(ctx.page);
+
+      const events = await ctx.page?.evaluate(
+        () => (window as unknown as IWindow).snapshots,
+      );
+
+      expect(events).toContainEqual(
+        expect.objectContaining({
+          type: EventType.Meta,
+          data: {
+            href: expect.any(String),
+            width: expect.any(Number),
+            height: expect.any(Number),
+            captureAssets: {
+              origins: true,
+              objectURLs: false,
+            },
+          },
+        }),
+      );
+    });
+
     it('capture all urls', async () => {
       await ctx.page.waitForNetworkIdle({ idleTime: 100 });
       await waitForRAF(ctx.page);
