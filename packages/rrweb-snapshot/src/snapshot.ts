5806ac0c2d4129099425e22579f46b403d68eccc
Michael Dellanoce <mdellanoce@pendo.io>
2023-12-08 09:47:09 -0500

    allow passing null to maskTextClass/blockClass

--- packages/rrweb-snapshot/src/snapshot.ts
+++ packages/rrweb-snapshot/src/snapshot.ts
@@ -271,7 +271,7 @@ export function _isBlockedElement(
       if (element.classList.contains(blockClass)) {
         return true;
       }
-    } else {
+    } else if (blockClass) {
       for (let eIndex = element.classList.length; eIndex--; ) {
         const className = element.classList[eIndex];
         if (blockClass.test(className)) {
@@ -295,6 +295,7 @@ export function classMatchesRegex(
   checkAncestors: boolean,
 ): boolean {
   if (!node) return false;
+  if (!regex) return false;
   if (node.nodeType !== node.ELEMENT_NODE) {
     if (!checkAncestors) return false;
     return classMatchesRegex(node.parentNode, regex, checkAncestors);
