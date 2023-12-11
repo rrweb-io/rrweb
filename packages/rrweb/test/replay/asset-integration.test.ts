ea0ea805153c2c824b815a695d6aec89194851d7
Justin Halsall <Juice10@users.noreply.github.com>
2023-12-11 14:50:45 +0100

    Add failing test for src attribute

--- packages/rrweb/test/replay/asset-integration.test.ts
+++ packages/rrweb/test/replay/asset-integration.test.ts
@@ -5,6 +5,8 @@ import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import type * as puppeteer from 'puppeteer';
 import events from '../events/assets';
 import mutationEvents from '../events/assets-mutation';
+import assetsChangedEvents from '../events/assets-src-changed-before-asset-loaded';
+import type { assetEvent } from '@rrweb/types';
 import { vi } from 'vitest';
 
 interface ISuite {
@@ -41,6 +43,9 @@ describe('replayer', function () {
     await page.evaluate(`let events = ${JSON.stringify(events)}`);
     await page.evaluate(
       `let mutationEvents = ${JSON.stringify(mutationEvents)}`,
+    );
+    await page.evaluate(
+      `let assetsChangedEvents = ${JSON.stringify(assetsChangedEvents)}`,
     );
 
     page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
@@ -116,13 +121,154 @@ describe('replayer', function () {
       expect(image).toMatchImageSnapshot();
     });
 
-    test.todo("should support work through rrelement's too");
-    test.todo('should support video elements');
-    test.todo('should support audio elements');
-    test.todo('should support embed elements');
-    test.todo('should support source elements');
-    test.todo('should support track elements');
-    test.todo('should support input#type=image elements');
-    test.todo('should support img srcset');
+    it('on mutation should wait with adding src attribute until the asset is loaded', async () => {
+      await page.evaluate(`
+      const { Replayer } = rrweb;
+      window.replayer = new Replayer([], {
+        liveMode: true,
+      });
+      replayer.startLive(mutationEvents[0].timestamp);
+      window.replayer.addEvent(mutationEvents[0]);
+      window.replayer.addEvent(mutationEvents[1]);
+      window.replayer.addEvent(mutationEvents[2]);
+    `);
+
+      await waitForRAF(page);
+
+      expect(
+        await page.evaluate(
+          `document.querySelector('iframe').contentDocument.querySelector('img').getAttribute('src')`,
+        ),
+      ).toBe(null);
+
+      await page.evaluate(`
+        window.replayer.addEvent(mutationEvents[3]);
+      `);
+
+      await waitForRAF(page);
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot();
+    });
+
+    it('should wait with adding src attribute until the asset is loaded 2', async () => {
+      await page.evaluate(`
+      const { Replayer } = rrweb;
+      window.replayer = new Replayer([], {
+        liveMode: true,
+      });
+      replayer.startLive(events[0].timestamp);
+      window.replayer.addEvent(events[0]);
+      window.replayer.addEvent(events[1]);
+    `);
+
+      await waitForRAF(page);
+
+      expect(
+        await page.evaluate(
+          `document.querySelector('iframe').contentDocument.querySelector('img').getAttribute('src')`,
+        ),
+      ).toBe(null);
+
+      await page.evaluate(`
+        window.replayer.addEvent(events[2]);
+      `);
+
+      await waitForRAF(page);
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot();
+    });
+
+    it('should show the correct asset when assets are loading while src is changed in live mode', async () => {
+      await page.evaluate(`
+      const { Replayer } = rrweb;
+      window.replayer = new Replayer([], {
+        liveMode: true,
+      });
+      replayer.startLive(assetsChangedEvents[0].timestamp);
+      window.replayer.addEvent(assetsChangedEvents[0]);
+      window.replayer.addEvent(assetsChangedEvents[1]);
+      window.replayer.addEvent(assetsChangedEvents[2]);
+      window.replayer.addEvent(assetsChangedEvents[3]);
+      window.replayer.addEvent(assetsChangedEvents[4]);
+    `);
+
+      await waitForRAF(page);
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot();
+    });
+
+    it('should show the loaded asset (robot) in non-live mode', async () => {
+      await page.evaluate(`
+      const { Replayer } = rrweb;
+      window.replayer = new Replayer(assetsChangedEvents);
+      replayer.pause(assetsChangedEvents[1].timestamp);
+    `);
+
+      await waitForRAF(page);
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot();
+    });
+
+    it('should show the loaded asset (red square) in non-live mode', async () => {
+      await page.evaluate(`
+      const { Replayer } = rrweb;
+      window.replayer = new Replayer(assetsChangedEvents);
+      replayer.pause(assetsChangedEvents[2].timestamp);
+    `);
+
+      await waitForRAF(page);
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot();
+    });
+
+    it('should list original url in non-live mode when asset never gets loaded', async () => {
+      await page.evaluate(`
+      const { Replayer } = rrweb;
+      window.replayer = new Replayer([assetsChangedEvents[0], assetsChangedEvents[1]]);
+      replayer.pause(assetsChangedEvents[1].timestamp);
+    `);
+
+      await waitForRAF(page);
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot();
+    });
+
+    it('should list original url in non-live mode when asset fails to load', async () => {
+      const failedEvent: assetEvent & { timestamp: number } = {
+        type: 7,
+        data: {
+          url: 'ftp://example.com/original-image.png',
+          failed: {
+            status: 404,
+            message: 'Not Found',
+          },
+        },
+        timestamp: assetsChangedEvents[2].timestamp,
+      };
+      await page.evaluate(`
+      const { Replayer } = rrweb;
+      window.replayer = new Replayer([assetsChangedEvents[0], assetsChangedEvents[1], ${JSON.stringify(
+        failedEvent,
+      )}]);
+      replayer.pause(assetsChangedEvents[1].timestamp);
+    `);
+
+      await waitForRAF(page);
+
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot();
+
+      expect(
+        await page.evaluate(
+          `document.querySelector('iframe').contentDocument.querySelector('img').getAttribute('src')`,
+        ),
+      ).toMatchInlineSnapshot(`"ftp://example.com/original-image.png"`);
+    });
   });
 });
