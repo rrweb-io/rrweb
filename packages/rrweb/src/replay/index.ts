e8fc652541d3e2256e244be4c83d787f99abb1af
Justin Halsall <Juice10@users.noreply.github.com>
2023-12-14 13:26:25 +0100

    Change asset manager path

--- packages/rrweb/src/replay/index.ts
+++ packages/rrweb/src/replay/index.ts
@@ -87,7 +87,7 @@ import getInjectStyleRules from './styles/inject-style';
 import './styles/style.css';
 import canvasMutation from './canvas';
 import { deserializeArg } from './canvas/deserialize-args';
-import AssetManager from './assets';
+import AssetManager from './asset-manager';
 import { MediaManager } from './media';
 
 const SKIP_TIME_INTERVAL = 5 * 1000;
