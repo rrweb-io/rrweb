7b2d37e2ee2eff39798a6a5e960165c4c0284be2
Alexey Babik <alexey.babik@noibu.com>
2024-01-04 14:01:16 -0600

    Use maskInputFn for full snapshot

--- packages/rrweb-snapshot/src/snapshot.ts
+++ packages/rrweb-snapshot/src/snapshot.ts
@@ -1235,7 +1235,7 @@ function snapshot(
     inlineStylesheet?: boolean;
     maskAllInputs?: boolean | MaskInputOptions;
     maskTextFn?: MaskTextFn;
-    maskInputFn?: MaskTextFn;
+    maskInputFn?: MaskInputFn;
     slimDOM?: 'all' | boolean | SlimDOMOptions;
     dataURLOptions?: DataURLOptions;
     inlineImages?: boolean;
