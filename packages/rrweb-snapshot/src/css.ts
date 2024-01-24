99d334aa0db6c7d2e0d89a256f116ed8424d120f
Daniel Engelke <dengelke91@gmail.com>
2024-01-24 15:28:25 +0800

    Run format

--- packages/rrweb-snapshot/src/css.ts
+++ packages/rrweb-snapshot/src/css.ts
@@ -431,16 +431,21 @@ export function parse(css: string, options: ParserOptions = {}) {
    */
 
   function selector() {
+    whitespace();
+    while (css[0] == '}') {
+      error('extra closing bracket');
+      css = css.slice(1);
+      whitespace();
+    }
 
-    whitespace(); while(css[0] == '}'){ error('extra closing bracket'); css = css.slice(1); whitespace(); }
-
-		const m = match(/^(("(?:\\"|[^"])*"|'(?:\\'|[^'])*'|[^{])+)/);
+    const m = match(/^(("(?:\\"|[^"])*"|'(?:\\'|[^'])*'|[^{])+)/);
     if (!m) {
       return;
     }
     /* @fix Remove all comments from selectors
      * http://ostermiller.org/findcomment.html */
-    const cleanedInput = m[0].trim()
+    const cleanedInput = m[0]
+      .trim()
       .replace(/\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*\/+/g, '')
 
       // Handle strings by replacing commas inside them
@@ -448,47 +453,49 @@ export function parse(css: string, options: ParserOptions = {}) {
         return m.replace(/,/g, '\u200C');
       });
 
-      // Split using a custom function and restore commas in strings
-      return customSplit(cleanedInput).map(s => s.replace(/\u200C/g, ',').trim());
-    }
+    // Split using a custom function and restore commas in strings
+    return customSplit(cleanedInput).map((s) =>
+      s.replace(/\u200C/g, ',').trim(),
+    );
+  }
 
-    /**
-     * Split selector correctly, ensuring not to split on comma if inside ().
-     */
+  /**
+   * Split selector correctly, ensuring not to split on comma if inside ().
+   */
 
-    function customSplit(input: string) {
-      let result = [];
-      let currentSegment = '';
-      let depthParentheses = 0; // Track depth of parentheses
-      let depthBrackets = 0; // Track depth of square brackets
-    
-      for (let char of input) {
-        if (char === '(') {
-          depthParentheses++;
-        } else if (char === ')') {
-          depthParentheses--;
-        } else if (char === '[') {
-          depthBrackets++;
-        } else if (char === ']') {
-          depthBrackets--;
-        }
-    
-        // Split point is a comma that is not inside parentheses or square brackets
-        if (char === ',' && depthParentheses === 0 && depthBrackets === 0) {
-          result.push(currentSegment);
-          currentSegment = '';
-        } else {
-          currentSegment += char;
-        }
+  function customSplit(input: string) {
+    let result = [];
+    let currentSegment = '';
+    let depthParentheses = 0; // Track depth of parentheses
+    let depthBrackets = 0; // Track depth of square brackets
+
+    for (let char of input) {
+      if (char === '(') {
+        depthParentheses++;
+      } else if (char === ')') {
+        depthParentheses--;
+      } else if (char === '[') {
+        depthBrackets++;
+      } else if (char === ']') {
+        depthBrackets--;
       }
-    
-      // Add the last segment
-      if (currentSegment) {
+
+      // Split point is a comma that is not inside parentheses or square brackets
+      if (char === ',' && depthParentheses === 0 && depthBrackets === 0) {
         result.push(currentSegment);
+        currentSegment = '';
+      } else {
+        currentSegment += char;
       }
-    
-      return result;
     }
+
+    // Add the last segment
+    if (currentSegment) {
+      result.push(currentSegment);
+    }
+
+    return result;
+  }
 
   /**
    * Parse declaration.
