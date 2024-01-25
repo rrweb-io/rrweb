87c6b133a7aa033778b8295c83ec33332016e234
Paul D'Ambra <paul@posthog.com>
2024-01-25 08:22:16 +0000

    fix: console logger can serialize bigint values

---
+++ packages/rrweb/test/plugins/console/record.test.ts
@@ -0,0 +1,7 @@
+import { stringify } from '../../../src/plugins/console/record/stringify';
+
+describe('console record plugin', () => {
+  it('can stringify bigint', () => {
+    expect(stringify(BigInt(1))).toEqual('"1n"');
+  });
+});
