877ce0599d405440ed842ffda53c5bad8874e16c
Justin Halsall <Juice10@users.noreply.github.com>
2024-02-05 17:40:04 +0100

    Docs: add examples to `getUrlsFromSrcset`

--- packages/rrweb-snapshot/src/utils.ts
+++ packages/rrweb-snapshot/src/utils.ts
@@ -409,6 +409,11 @@ export function findCssTextSplits(
   return splits;
 }
 
+/**
+ * Extracts the URLs from a srcset attribute.
+ * @param srcset - The srcset attribute value. eg. `image.jpg 2x, image2.jpg 3x`
+ * @returns An array of URLs. eg. `['image.jpg', 'image2.jpg']`
+ */
 export function getUrlsFromSrcset(srcset: string): string[] {
   const urls: string[] = [];
   const parts = srcset.split(',');
@@ -417,11 +422,13 @@ export function getUrlsFromSrcset(srcset: string): string[] {
     const spaceIndex = trimmed.indexOf(' ');
     if (spaceIndex === -1) {
       // If no descriptor is specified, it's a single URL.
+      // eg. `image.jpg`
       urls.push(trimmed);
     } else {
-      // Otherwise, it's one or more URLs followed by a single descriptor.
+      // Otherwise, it's a URL followed by a single descriptor.
       // Since we don't know how long the URL will be, we'll assume it's everything
-      // after the first space.
+      // before the first space.
+      // eg. `image.jpg 2x`
       urls.push(trimmed.substring(0, spaceIndex));
     }
   }
