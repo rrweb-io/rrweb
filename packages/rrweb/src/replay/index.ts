24ab3047cd2809484dc964080dfb837c91ec5e1a
Eoghan Murray <eoghan@getthere.ie>
2024-03-27 14:12:41 +0000

    Revert attributes in non-live mode and ensure this happens when we can determine that Assets won't be arriving later

--- packages/rrweb/src/replay/index.ts
+++ packages/rrweb/src/replay/index.ts
@@ -849,7 +849,7 @@ export class Replayer {
       }
     };
 
-    void this.preloadAllAssets(event.timestamp);
+    void this.preloadAllAssets(event.timestamp, this.config.liveMode);
 
     /**
      * Normally rebuilding full snapshot should not be under virtual dom environment.
@@ -1072,7 +1072,10 @@ export class Replayer {
   /**
    * Process all asset events and preload them
    */
-  private async preloadAllAssets(timestamp: number): Promise<void[]> {
+  private async preloadAllAssets(
+    timestamp: number,
+    liveMode: boolean,
+  ): Promise<void[]> {
     const promises: Promise<void>[] = [];
     for (const event of this.service.state.context.events) {
       if (event.timestamp <= timestamp) continue;
@@ -1080,6 +1083,9 @@ export class Replayer {
       if (event.type === EventType.Asset) {
         promises.push(this.assetManager.add(event));
       }
+    }
+    if (!liveMode) {
+      this.assetManager.allAdded = true;
     }
     return Promise.all(promises);
   }
