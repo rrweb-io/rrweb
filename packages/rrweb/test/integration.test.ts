90af2c6559606a7e26bb7986b869bd03e3f24d21
Eoghan Murray <eoghan@getthere.ie>
2024-03-28 09:33:24 +0000

    Bump timeouts here as I was getting failures when running whole test suite concurrently

--- packages/rrweb/test/integration.test.ts
+++ packages/rrweb/test/integration.test.ts
@@ -867,7 +867,7 @@ describe('record integration tests', function (this: ISuite) {
       }),
     );
     await page.waitForResponse(`${serverURL}/html/assets/robot.png`);
-    await page.waitForTimeout(50); // wait for image to get added
+    await page.waitForTimeout(150); // wait for image to get added
     await waitForRAF(page); // wait for image to be captured
 
     const snapshots = (await page.evaluate(
@@ -893,7 +893,7 @@ describe('record integration tests', function (this: ISuite) {
       iframe.setAttribute('src', '/html/image-blob-url.html');
     });
     await page.waitForResponse(`${serverURL}/html/assets/robot.png`); // wait for image to get loaded
-    await page.waitForTimeout(50); // wait for image to get added
+    await page.waitForTimeout(150); // wait for image to get added
     await waitForRAF(page); // wait for image to be captured
 
     const snapshots = (await page.evaluate(
@@ -931,7 +931,7 @@ describe('record integration tests', function (this: ISuite) {
       }),
     );
     await page.waitForResponse(`${serverURL}/html/assets/robot.png`);
-    await page.waitForTimeout(50); // wait for image to get added
+    await page.waitForTimeout(150); // wait for image to get added
     await waitForRAF(page); // wait for image to be captured
 
     const snapshots = (await page.evaluate(
@@ -956,7 +956,7 @@ describe('record integration tests', function (this: ISuite) {
       iframe.setAttribute('src', '/html/image-blob-url.html');
     });
     await page.waitForResponse(`${serverURL}/html/assets/robot.png`); // wait for image to get loaded
-    await page.waitForTimeout(50); // wait for image to get added
+    await page.waitForTimeout(150); // wait for image to get added
     await waitForRAF(page); // wait for image to be captured
 
     const snapshots = (await page.evaluate(
