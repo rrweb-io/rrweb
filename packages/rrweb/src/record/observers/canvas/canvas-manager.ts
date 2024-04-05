3d1877cff83d9a018630674fb6e730050ceef812
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2024-04-05 14:24:25 +0200

    fix: createImageBitmap throws DOMException if source is 0 (#1422)
    * fix: createImageBitmap throws DOMException if source is 0
    
    * code review
    
    * fix
    
    * Create breezy-cats-heal.md
    
    * fix format
    
    ---------
    
    Co-authored-by: Justin Halsall <Juice10@users.noreply.github.com>

--- packages/rrweb/src/record/observers/canvas/canvas-manager.ts
+++ packages/rrweb/src/record/observers/canvas/canvas-manager.ts
@@ -184,6 +184,12 @@ export class CanvasManager {
         .forEach(async (canvas: HTMLCanvasElement) => {
           const id = this.mirror.getId(canvas);
           if (snapshotInProgressMap.get(id)) return;
+
+          // The browser throws if the canvas is 0 in size
+          // Uncaught (in promise) DOMException: Failed to execute 'createImageBitmap' on 'Window': The source image width is 0.
+          // Assuming the same happens with height
+          if (canvas.width === 0 || canvas.height === 0) return;
+
           snapshotInProgressMap.set(id, true);
           if (['webgl', 'webgl2'].includes((canvas as ICanvas).__context)) {
             // if the canvas hasn't been modified recently,
