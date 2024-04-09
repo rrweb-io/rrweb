71450e2556a95573ee0d5e7f75b0aabda09298ab
Justin Halsall <Juice10@users.noreply.github.com>
2024-04-09 12:43:16 +0200

    Get tests to work in rrdom-nodejs

--- packages/rrdom-nodejs/jest.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
---
+++ vitest.workspace.ts
@@ -0,0 +1,5 @@
+export default [
+  'packages/rrdom-nodejs',
+  'packages/packer',
+  'packages/plugins/rrweb-plugin-console-record',
+];
