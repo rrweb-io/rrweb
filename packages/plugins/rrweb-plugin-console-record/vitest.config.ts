e46a95180ff937a7a1818d44001eca023df5089f
Justin Halsall <Juice10@users.noreply.github.com>
2024-04-10 15:33:48 +0200

    Fix publint and attw issues for packages

---
+++ packages/plugins/rrweb-plugin-console-record/vitest.config.ts
@@ -0,0 +1,5 @@
+/// <reference types="vitest" />
+import { defineProject, mergeConfig } from 'vitest/config';
+import configShared from '../../../vitest.config.ts';
+
+export default mergeConfig(configShared, defineProject({}));
