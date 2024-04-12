6ec938330c9eeff4fb8d5a7e091b12bd49893bd0
Eoghan Murray <eoghan@getthere.ie>
2024-04-12 18:00:19 +0100

    Add test and remedy for the most trivial type of failure of `findCssTextSplits`

--- packages/rrweb-snapshot/test/css.test.ts
+++ packages/rrweb-snapshot/test/css.test.ts
@@ -1,6 +1,11 @@
+/**
+ * @vitest-environment jsdom
+ */
 import { describe, it, expect } from 'vitest';
 import { mediaSelectorPlugin, pseudoClassPlugin } from '../src/css';
 import postcss, { AcceptedPlugin } from 'postcss';
+import { JSDOM } from 'jsdom';
+import { findCssTextSplits, stringifyStylesheet } from './../src/utils';
 
 describe('css parser', () => {
   function parse(plugin: AcceptedPlugin, input: string): string {
@@ -71,5 +76,26 @@ li[attr="weirder\\"("] a.\\:hover {background-color: red;}`);
 li[attr="has,comma"] a.\\:hover {background: red;}`,
       );
     });
+  });
+});
+
+describe('css splitter', () => {
+  it('finds css textElement splits correctly', () => {
+    const style = JSDOM.fragment(`<style></style>`).querySelector('style');
+    if (style) {
+      // as authored, e.g. no spaces
+      style.appendChild(JSDOM.fragment('.a{background-color:red;}'));
+      style.appendChild(JSDOM.fragment('.a{background-color:black;}'));
+
+      // how it is currently stringified (spaces present)
+      let browserSheet = '.a { background-color: red; }';
+      let expectedSplit = browserSheet.length;
+      browserSheet += '.a { background-color: black; }';
+
+      // can't do this as JSDOM doesn't have style.sheet
+      //expect(stringifyStylesheet(style.sheet!)).toEqual(browserSheet);
+
+      expect(findCssTextSplits(browserSheet, style)).toEqual([expectedSplit]);
+    }
   });
 });
