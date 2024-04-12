324a7cfdb79879186f46c2b272ea595786c59ae4
Justin Halsall <Juice10@users.noreply.github.com>
2024-04-12 17:00:56 +0200

    Add minification and umd for all built files

--- packages/rrweb/vite.config.entries.js
+++ packages/rrweb/vite.config.entries.js
@@ -2,9 +2,11 @@ import config from '../../vite.config.default';
 
 export default config(
   {
+    // rrweb: 'src/index.ts',
     'rrweb-record': 'src/entries/record.ts',
     'rrweb-replay': 'src/entries/replay.ts',
   },
   'rrweb',
-  { outputDir: 'dist/alt' },
+  // { outputDir: 'dist/alt' },
+  { outputDir: 'dist' },
 );
