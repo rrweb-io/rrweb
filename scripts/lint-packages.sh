cd71e9300f7258c8f373123b4eff5f1f942ac5aa
Justin Halsall <Juice10@users.noreply.github.com>
2024-04-14 12:15:43 +0200

    Add script to lint packages

---
+++ scripts/lint-packages.sh
@@ -0,0 +1,12 @@
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+packages_dir="$script_dir/../packages"
+
+for dir in "$packages_dir"/*/ "$packages_dir/plugins"/*/ ; do
+    if [ -d "$dir" ] && [ -f "$dir/package.json" ]; then
+        (
+            cd "$dir" || exit
+            npx publint --strict
+            attw --pack .
+        )
+    fi
+done
