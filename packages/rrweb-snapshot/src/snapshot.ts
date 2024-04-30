0dffc73dbde678d2e2f458428d9e4e1001ed4363
Eoghan Murray <eoghan@getthere.ie>
2024-04-30 15:10:22 +0100

    Allow certain assets to be captured synchronously during mutation

--- packages/rrweb-snapshot/src/snapshot.ts
+++ packages/rrweb-snapshot/src/snapshot.ts
@@ -685,6 +685,7 @@ function serializeElementNode(
     captureAssets = {
       objectURLs: true,
       origins: false,
+      _fromMutation: false,
     },
     recordCanvas,
     keepIframeSrcFn,
@@ -699,14 +700,13 @@ function serializeElementNode(
 
   // legacy, the stringifyStylesheet badly blocks the main thread as web page loads when taking an initial snapshot
   // prefer to capture as an asset instead
-  if (tagName === 'link' && !onAssetDetected) {
+  if (
+    tagName === 'link' &&
+    inlineStylesheet &&
+    (!onAssetDetected || captureAssets._fromMutation)
+  ) {
     const l = n as HTMLLinkElement;
-    if (
-      inlineStylesheet &&
-      l.href &&
-      lowerIfExists(l.rel) === 'stylesheet' &&
-      l.sheet
-    ) {
+    if (l.href && lowerIfExists(l.rel) === 'stylesheet' && l.sheet) {
       const cssText = stringifyStylesheet(l.sheet);
       if (cssText) {
         attributes._cssText = absoluteToStylesheet(cssText, l.href);
@@ -745,7 +745,7 @@ function serializeElementNode(
   }
   if (tagName === 'style' && (n as HTMLStyleElement).sheet) {
     const sheetBaseHref = getHref(doc);
-    if (!onAssetDetected) {
+    if (!onAssetDetected || captureAssets._fromMutation) {
       let cssText = stringifyStylesheet(
         (n as HTMLStyleElement).sheet as CSSStyleSheet,
       );
