dc3c22ad9e06521dc7235033c9a64e27a3a9d11a
dkindel <dkindel@users.noreply.github.com>
2024-05-01 20:16:15 +0000

    Apply formatting changes

--- packages/rrweb-snapshot/test/css.test.ts
+++ packages/rrweb-snapshot/test/css.test.ts
@@ -154,7 +154,7 @@ describe('css parser', () => {
 
   it('parses nested commas in selectors correctly', () => {
     const result = parse(
-        `
+      `
 body > ul :is(li:not(:first-of-type) a:hover, li:not(:first-of-type).active a) {
   background: red;
 }
@@ -163,36 +163,36 @@ body > ul :is(li:not(:first-of-type) a:hover, li:not(:first-of-type).active a) {
     expect((result.stylesheet!.rules[0] as Rule)!.selectors!.length).toEqual(1);
 
     const trickresult = parse(
-        `
+      `
 li[attr="weirdly("] a:hover, li[attr="weirdly)"] a {
   background-color: red;
 }
 `,
     );
     expect(
-        (trickresult.stylesheet!.rules[0] as Rule)!.selectors!.length,
+      (trickresult.stylesheet!.rules[0] as Rule)!.selectors!.length,
     ).toEqual(2);
 
     const weirderresult = parse(
-        `
+      `
 li[attr="weirder\\"("] a:hover, li[attr="weirder\\")"] a {
   background-color: red;
 }
 `,
     );
     expect(
-        (weirderresult.stylesheet!.rules[0] as Rule)!.selectors!.length,
+      (weirderresult.stylesheet!.rules[0] as Rule)!.selectors!.length,
     ).toEqual(2);
 
     const commainstrresult = parse(
-        `
+      `
 li[attr="has,comma"] a:hover {
   background-color: red;
 }
 `,
     );
     expect(
-        (commainstrresult.stylesheet!.rules[0] as Rule)!.selectors!.length,
+      (commainstrresult.stylesheet!.rules[0] as Rule)!.selectors!.length,
     ).toEqual(1);
   });
 
