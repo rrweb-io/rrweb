591d9b350a7544925bd49e97a042e64c9cfbd807
Colin Maxfield <colinmaxfield@gmail.com>
2024-05-17 11:32:02 -0400

    Replace Array.from with clean implementation (#1464)
    This work is to try to provide support where rrweb might be included
    in applications with various tools that might override Array.from
    so that the 2nd parameter (the map function) will always work for
    rrweb.
    
    Co-authored-by: Michael Dellanoce <mdellanoce@pendo.io>

--- packages/rrweb/src/record/index.ts
+++ packages/rrweb/src/record/index.ts
@@ -68,6 +68,20 @@ let _wrappedEmit:
   | ((e: eventWithTime, isCheckout?: boolean) => void);
 let _takeFullSnapshot: undefined | ((isCheckout?: boolean) => void);
 
+// Multiple tools (i.e. MooTools, Prototype.js) override Array.from and drop support for the 2nd parameter
+// Try to pull a clean implementation from a newly created iframe
+try {
+  if (Array.from([1], (x) => x * 2)[0] !== 2) {
+    const cleanFrame = document.createElement('iframe');
+    document.body.appendChild(cleanFrame);
+    // eslint-disable-next-line @typescript-eslint/unbound-method -- Array.from is static and doesn't rely on binding
+    Array.from = cleanFrame.contentWindow?.Array.from || Array.from;
+    document.body.removeChild(cleanFrame);
+  }
+} catch (err) {
+  console.debug('Unable to override Array.from', err);
+}
+
 export const mirror = createMirror();
 
 function record<T = eventWithTime>(
