607058098fc866cf236160fd2a312c9ab74a7301
Billy Vong <billyvg@users.noreply.github.com>
2024-05-29 11:00:21 -0400

    feat(ci): Add `Replayer` to size limit check
    Also make sure we run the action for branch

--- .size-limit.js
+++ .size-limit.js
@@ -4,19 +4,19 @@ module.exports = [
     name: 'rrweb - record only (gzipped)',
     path: 'packages/rrweb/es/rrweb/packages/rrweb/src/entries/all.js',
     import: '{ record }',
-    gzip: true
+    gzip: true,
   },
   {
     name: 'rrweb - record & CanvasManager only (gzipped)',
     path: 'packages/rrweb/es/rrweb/packages/rrweb/src/entries/all.js',
     import: '{ record, CanvasManager }',
-    gzip: true
+    gzip: true,
   },
   {
     name: 'rrweb - record only (min)',
     path: 'packages/rrweb/es/rrweb/packages/rrweb/src/entries/all.js',
     import: '{ record }',
-    gzip: false
+    gzip: false,
   },
   {
     name: 'rrweb - record with treeshaking flags (gzipped)',
@@ -34,5 +34,10 @@ module.exports = [
       return config;
     },
   },
- 
+  {
+    name: 'rrweb - Replayer',
+    path: 'packages/rrweb/es/rrweb/packages/rrweb/src/entries/all.js',
+    import: '{ Replayer }',
+    gzip: true,
+  },
 ];
