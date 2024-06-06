ec2e864fd55fd8f6287c52e14103e6bbc7517bed
jackson-amplitude <jackson-amplitude@users.noreply.github.com>
2024-06-06 01:02:41 +0000

    Apply formatting changes

--- packages/rrweb/src/record/error-handler.ts
+++ packages/rrweb/src/record/error-handler.ts
@@ -2,7 +2,7 @@ import type { ErrorHandler } from '../types';
 
 type Callback = (...args: unknown[]) => unknown;
 type GenericFunction = (...args: any[]) => any;
-type ExternalError = Error & {_external_: boolean}
+type ExternalError = Error & { _external_: boolean };
 
 let errorHandler: ErrorHandler | undefined;
 
@@ -37,17 +37,19 @@ export const callbackWrapper = <T extends Callback>(cb: T): T => {
   return rrwebWrapped;
 };
 
-export function externalFunctionWrapper<T extends GenericFunction>(func: T): (...args: Parameters<T>) => ReturnType<T> {
+export function externalFunctionWrapper<T extends GenericFunction>(
+  func: T,
+): (...args: Parameters<T>) => ReturnType<T> {
   return (...args: Parameters<T>) => {
     try {
       return func(...args);
     } catch (error) {
-        try {
-            (error as ExternalError)._external_ = true;
-        } catch {
-          // in case we can't assign, don't do anything.
-        }
-        throw error
+      try {
+        (error as ExternalError)._external_ = true;
+      } catch {
+        // in case we can't assign, don't do anything.
+      }
+      throw error;
+    }
   };
-}
 }
