f9c4f6c2f9fd70162a8850ed25c83922c65fea72
Juice10 <Juice10@users.noreply.github.com>
2024-06-09 09:11:07 +0000

    Apply formatting changes

--- packages/rrweb-player/.svelte-kit/ambient.d.ts
+++ packages/rrweb-player/.svelte-kit/ambient.d.ts
@@ -1,24 +1,26 @@
+
 // this file is generated — do not edit it
+
 
 /// <reference types="@sveltejs/kit" />
 
 /**
  * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
- *
+ * 
  * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
- *
+ * 
  * ```ts
  * import { API_KEY } from '$env/static/private';
  * ```
- *
+ * 
  * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
- *
+ * 
  * ```
  * MY_FEATURE_FLAG=""
  * ```
- *
+ * 
  * You can override `.env` values from the command line like so:
- *
+ * 
  * ```bash
  * MY_FEATURE_FLAG="enabled" npm run dev
  * ```
@@ -241,27 +243,29 @@ declare module '$env/static/private' {
 
 /**
  * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
- *
+ * 
  * Values are replaced statically at build time.
- *
+ * 
  * ```ts
  * import { PUBLIC_BASE_URL } from '$env/static/public';
  * ```
  */
-declare module '$env/static/public' {}
+declare module '$env/static/public' {
+	
+}
 
 /**
  * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
- *
+ * 
  * This module cannot be imported into client-side code.
- *
+ * 
  * Dynamic environment variables cannot be used during prerendering.
- *
+ * 
  * ```ts
  * import { env } from '$env/dynamic/private';
  * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
  * ```
- *
+ * 
  * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
  */
 declare module '$env/dynamic/private' {
@@ -486,18 +490,18 @@ declare module '$env/dynamic/private' {
 
 /**
  * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
- *
+ * 
  * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
- *
+ * 
  * Dynamic environment variables cannot be used during prerendering.
- *
+ * 
  * ```ts
  * import { env } from '$env/dynamic/public';
  * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
  * ```
  */
 declare module '$env/dynamic/public' {
-  export const env: {
-    [key: `PUBLIC_${string}`]: string | undefined;
-  };
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
 }
