e08706ae60268b6eb05c6292ef948c71bd423ce3
Eoghan Murray <eoghan@getthere.ie>
2024-06-13 06:27:39 +0100

    Add option to block animation on <title> tag (#760)
    * Add option to block animation on <title> tag which can generate massive recordings on some websites (think scrolling title tag)
    
    * Add new option to slimDOMOptions type with tsdoc as suggested by Justin

--- packages/rrweb/src/record/index.ts
+++ packages/rrweb/src/record/index.ts
@@ -174,6 +174,7 @@ function record<T = eventWithTime>(
           // as they destroy some (hidden) info:
           headMetaAuthorship: _slimDOMOptions === 'all',
           headMetaDescKeywords: _slimDOMOptions === 'all',
+          headTitleMutations: _slimDOMOptions === 'all',
         }
       : _slimDOMOptions
       ? _slimDOMOptions
