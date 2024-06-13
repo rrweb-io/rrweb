2bd51a38178307a8045c9a3560311dd40f8af3ab
Eoghan Murray <eoghan@getthere.ie>
2024-06-13 06:27:39 +0100

    Add option to block animation on <title> tag (#760)
    * Add option to block animation on <title> tag which can generate massive recordings on some websites (think scrolling title tag)
    
    * Add new option to slimDOMOptions type with tsdoc as suggested by Justin

--- packages/rrweb/src/record/mutation.ts
+++ packages/rrweb/src/record/mutation.ts
@@ -567,7 +567,7 @@ export default class MutationBuffer {
   };
 
   private processMutation = (m: mutationRecord) => {
-    if (isIgnored(m.target, this.mirror)) {
+    if (isIgnored(m.target, this.mirror, this.slimDOMOptions)) {
       return;
     }
     switch (m.type) {
@@ -733,7 +733,7 @@ export default class MutationBuffer {
             : this.mirror.getId(m.target);
           if (
             isBlocked(m.target, this.blockClass, this.blockSelector, false) ||
-            isIgnored(n, this.mirror) ||
+            isIgnored(n, this.mirror, this.slimDOMOptions) ||
             !isSerialized(n, this.mirror)
           ) {
             return;
@@ -793,7 +793,7 @@ export default class MutationBuffer {
     if (this.addedSet.has(n) || this.movedSet.has(n)) return;
 
     if (this.mirror.hasNode(n)) {
-      if (isIgnored(n, this.mirror)) {
+      if (isIgnored(n, this.mirror, this.slimDOMOptions)) {
         return;
       }
       this.movedSet.add(n);
