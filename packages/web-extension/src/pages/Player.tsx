e0ae67113ad4b8f1be6e70b653bc5614f2325c54
reemhagbi <reem@saola.ai>
2024-06-16 15:53:51 +0300

    build pass

--- packages/web-extension/src/pages/Player.tsx
+++ packages/web-extension/src/pages/Player.tsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Replayer from 'rrweb-player';
+import Replayer from '@saola.ai/rrweb-player';
 import {
   Box,
   Breadcrumb,
