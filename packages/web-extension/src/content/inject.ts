7d4dc210d094614984fb4bf3cd905b72fc7d453d
Justin Halsall <Juice10@users.noreply.github.com>
2024-06-19 13:57:25 +0200

    Correct type imports for web-extensions package

--- packages/web-extension/src/content/inject.ts
+++ packages/web-extension/src/content/inject.ts
@@ -1,7 +1,7 @@
 import { record } from 'rrweb';
 import type { recordOptions } from 'rrweb';
 import type { eventWithTime } from '@rrweb/types';
-import { MessageName, RecordStartedMessage } from '~/types';
+import { MessageName, type RecordStartedMessage } from '~/types';
 import { isInCrossOriginIFrame } from '~/utils';
 
 /**
