f02b518505a80deec05d1eac65867b3283ac70fe
Paul D'Ambra <paul@posthog.com>
2024-07-02 15:19:10 +0100

    even more prettier

--- packages/plugins/rrweb-plugin-console-record/test/stringify.test.ts
+++ packages/plugins/rrweb-plugin-console-record/test/stringify.test.ts
@@ -6,7 +6,7 @@ import { stringify } from '../src/stringify';
 import { describe, it, expect } from 'vitest';
 
 describe('console record plugin', () => {
-  const aLongString = "a".repeat(100);
+  const aLongString = 'a'.repeat(100);
 
   it('can stringify bigint', () => {
     expect(stringify(BigInt(1))).toEqual('"1n"');
@@ -14,13 +14,20 @@ describe('console record plugin', () => {
 
   it('does not truncate by default', () => {
     expect(stringify(aLongString)).toEqual(`"${aLongString}"`);
-  })
+  });
 
   it('truncates when a length limit is provided', () => {
-    expect(stringify(aLongString, { stringLengthLimit: 10 })).toEqual(`"${"a".repeat(10)}..."`);
-  })
+    expect(stringify(aLongString, { stringLengthLimit: 10 })).toEqual(
+      `"${'a'.repeat(10)}..."`,
+    );
+  });
 
   it('truncates with specified suffix when a length limit is provided', () => {
-    expect(stringify(aLongString, { stringLengthLimit: 10, truncationSuffix: '...[truncated]' })).toEqual(`"${"a".repeat(10)}...[truncated]"`);
-  })
+    expect(
+      stringify(aLongString, {
+        stringLengthLimit: 10,
+        truncationSuffix: '...[truncated]',
+      }),
+    ).toEqual(`"${'a'.repeat(10)}...[truncated]"`);
+  });
 });
