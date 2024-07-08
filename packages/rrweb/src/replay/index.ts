02a50bcf475ac3e7105c7103f723cfb3c122a404
eoghanmurray <eoghanmurray@users.noreply.github.com>
2024-07-08 16:59:12 +0000

    Apply formatting changes

--- packages/rrweb/src/replay/index.ts
+++ packages/rrweb/src/replay/index.ts
@@ -1592,7 +1592,7 @@ export class Replayer {
         ) {
           // https://github.com/rrweb-io/rrweb/pull/1417
           /**
-           * If both _cssText and textContent are present for a style element due to some existing bugs, the element was ending up with two child text nodes 
+           * If both _cssText and textContent are present for a style element due to some existing bugs, the element was ending up with two child text nodes
            * We need to remove the textNode created by _cssText as it doesn't have an id in the mirror, and thus cannot be further mutated.
            */
           for (const cssText of prospectiveSiblings as (Node & RRNode)[]) {
