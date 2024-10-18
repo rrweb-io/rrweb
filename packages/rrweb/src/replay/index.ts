51969cfb82d0acb2813a79c66f26117a64398216
Eoghan Murray <eoghan@getthere.ie>
2024-10-18 15:43:37 +0100

    Mark where I think the assetManager resets were supposed to happen as I'm about to remove the reset in favour of keeping assets around

--- packages/rrweb/src/replay/index.ts
+++ packages/rrweb/src/replay/index.ts
@@ -408,6 +408,7 @@ export class Replayer {
     );
     if (firstMeta) {
       const { width, height } = firstMeta.data as metaEvent['data'];
+      this.assetManager.reset();
       setTimeout(() => {
         this.emitter.emit(ReplayerEvents.Resize, {
           width,
@@ -698,6 +699,7 @@ export class Replayer {
         break;
       case EventType.Meta:
         castFn = () => {
+          this.assetManager.reset();
           this.emitter.emit(ReplayerEvents.Resize, {
             width: event.data.width,
             height: event.data.height,
