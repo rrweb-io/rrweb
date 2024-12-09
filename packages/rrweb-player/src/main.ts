32e9577a902efb0be0f468a688d938d74d40542a
yunfeng0817 <yun.feng0817@gmail.com>
2024-12-09 00:42:25 -0800

    Revert "fix rrweb-player dts bug"
    This reverts commit 4f0bf98a9f3f0a6d3ffcf1300511023e48b1845b.

--- packages/rrweb-player/src/main.ts
+++ packages/rrweb-player/src/main.ts
@@ -1,10 +1,6 @@
 import _Player from './Player.svelte';
 import type { RRwebPlayerOptions } from './types';
-
-// Make sure generated Player type inherit all the props of _Player
-type SvelteComponentProps = InstanceType<typeof _Player>;
-
-export class Player extends _Player implements SvelteComponentProps {
+export class Player extends _Player {
   constructor(
     options: {
       // for compatibility
