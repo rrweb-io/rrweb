4f0bf98a9f3f0a6d3ffcf1300511023e48b1845b
yunfeng0817 <yun.feng0817@gmail.com>
2024-12-08 23:46:01 -0800

    fix rrweb-player dts bug

--- packages/rrweb-player/src/main.ts
+++ packages/rrweb-player/src/main.ts
@@ -1,6 +1,10 @@
 import _Player from './Player.svelte';
 import type { RRwebPlayerOptions } from './types';
-export class Player extends _Player {
+
+// Make sure generated Player type inherit all the props of _Player
+type SvelteComponentProps = InstanceType<typeof _Player>;
+
+export class Player extends _Player implements SvelteComponentProps {
   constructor(
     options: {
       // for compatibility
