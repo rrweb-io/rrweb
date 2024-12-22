e16e2ac20bf1b4338e4bf351a72ed5b800a5f040
Eoghan Murray <eoghan@getthere.ie>
2024-12-22 18:38:14 +0000

    Keep searching through https://github.com/puppeteer/puppeteer/blob/5d72c7d3c3e9c8cd53bcb4b597a0924dced1b5fc/docs/supported-browsers.md

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -6,5 +6,5 @@ const { join } = require('path');
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  browserRevision: '128.0.6613.119',
+  browserRevision: '130.0.6723.58',
 };
