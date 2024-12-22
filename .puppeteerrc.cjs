c43d695cff31c2cb9ca35558a01da5fe91a5bbad
Eoghan Murray <eoghan@getthere.ie>
2024-12-22 18:33:19 +0000

    Still getting 'zygote_host_impl_linux.cc(128)] No usable sandbox!' so iterating through versions

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -6,5 +6,5 @@ const { join } = require('path');
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  browserRevision: '127.0.6533.88',
+  browserRevision: '128.0.6613.119',
 };
