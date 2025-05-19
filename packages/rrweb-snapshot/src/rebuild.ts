708c532c7d619a78475f09035f098fcc570b9cb7
Ole Sund <ole.r.sund@gmail.com>
2025-05-19 07:46:40 +0200

    debug

--- packages/rrweb-snapshot/src/rebuild.ts
+++ packages/rrweb-snapshot/src/rebuild.ts
@@ -260,6 +260,10 @@ function buildNode(
          */
         if (value === true) value = '';
 
+        if (!name) {
+          console.log('name is null');
+        }
+
         if (name.startsWith('rr_')) {
           specialAttributes[name] = value;
           continue;
@@ -354,6 +358,11 @@ function buildNode(
             (node as unknown as RRCanvasElement).rr_dataURL = value.toString();
         } else if (tagName === 'img' && name === 'rr_dataURL') {
           const image = node as HTMLImageElement;
+
+          if (!image.currentSrc) {
+            console.log('image.currentSrc is null');
+          }
+
           if (!image.currentSrc.startsWith('data:')) {
             // Backup original img src. It may not have been set yet.
             image.setAttribute(
