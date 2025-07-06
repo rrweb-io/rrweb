83e3264054d917b35c88c9320e3b4519a117a2d1
Ole Sund <ole.r.sund@gmail.com>
2025-07-06 12:46:53 +0200

    same

---
+++ packages/rrweb-snapshot/src/asyncStylesheetManager.ts
@@ -0,0 +1,161 @@
+import { stringifyStylesheet } from './utils';
+
+const CLEANUP_DEBOUNCE_TIME = 1000 * 60 * 5;
+
+class AsyncStylesheetManager {
+  static instance: AsyncStylesheetManager;
+
+  constructor() {
+    if (AsyncStylesheetManager.instance) return AsyncStylesheetManager.instance;
+    AsyncStylesheetManager.instance = this;
+  }
+
+  private currentHref: null | string = null;
+
+  private clones: Record<
+    string,
+    | {
+        original: HTMLLinkElement;
+        clone: HTMLLinkElement;
+        loaded: boolean;
+        cssText: string | null;
+      }
+    | undefined
+  > = {};
+
+  private cleanTimeout: ReturnType<typeof setTimeout> | null = null;
+
+  onLoad(href: string) {
+    if (!(href in this.clones) || this.clones[href] === undefined) return;
+
+    console.log('AsyncStylesheetManager, onLoad: href:', href);
+
+    const styleSheets = Array.from(document.styleSheets);
+
+    let clonedStyleSheet: CSSStyleSheet | null = null;
+
+    document.head.removeChild(this.clones[href].clone);
+
+    //looping backwards in case the original did create a stylesheet (we want the clone)
+    for (let i = styleSheets.length - 1; i >= 0; i--) {
+      if (styleSheets[i].href === href) {
+        clonedStyleSheet = styleSheets[i];
+        break;
+      }
+    }
+
+    if (!clonedStyleSheet) return;
+
+    const newCssText = stringifyStylesheet(clonedStyleSheet);
+
+    if (!newCssText) return;
+
+    console.log(
+      'AsyncStylesheetManager, onLoad: success! did get new css text! forcing mutation...',
+    );
+
+    this.clones[href].cssText = newCssText;
+    this.clones[href].loaded = true;
+
+    //trigger a mutation on the original link element
+    this.clones[href].original.setAttribute(
+      'data-rrweb-mutation',
+      Date.now().toString(),
+    );
+  }
+
+  onLoadError(href: string) {
+    if (!(href in this.clones) || this.clones[href] === undefined) return;
+    document.head.removeChild(this.clones[href].clone);
+    //we'll keep the clone in memory, we still dont want to re-fetch it bc it will just fail again.
+  }
+
+  removeAllCloneElements() {
+    for (const clone of Object.values(this.clones)) {
+      if (!clone) continue;
+      document.head.removeChild(clone.clone);
+    }
+  }
+
+  onCleanTimeout() {
+    console.log('AsyncStylesheetManager, onCleanTimeout: cleaning up');
+
+    this.cleanTimeout = null;
+    this.removeAllCloneElements();
+  }
+
+  blowCache() {
+    console.log('AsyncStylesheetManager, blowCache: blowing cache');
+
+    this.clones = {};
+    this.removeAllCloneElements();
+  }
+
+  registerClone({ forElement }: { forElement: HTMLLinkElement }) {
+    if (this.currentHref != null && document.location.href !== this.currentHref)
+      this.blowCache();
+
+    this.currentHref = document.location.href;
+
+    const href = forElement.href;
+
+    console.log(
+      'AsyncStylesheetManager, registerClone: wants a clone for href:',
+      href,
+    );
+
+    if (!href) return;
+
+    if (href in this.clones && this.clones[href] !== undefined) return;
+
+    if (forElement.getAttribute('crossorigin') === 'anonymous') return;
+
+    console.log(
+      'AsyncStylesheetManager, registerClone: registering clone for href:',
+      href,
+    );
+
+    const clone = forElement.cloneNode() as HTMLLinkElement;
+    clone.setAttribute('crossorigin', 'anonymous');
+    clone.setAttribute('data-rrweb-link-cloned', 'true');
+
+    document.head.appendChild(clone);
+
+    this.clones[href] = {
+      original: forElement,
+      clone,
+      loaded: false,
+      cssText: null,
+    };
+
+    clone.onload = () => {
+      this.onLoad(href);
+    };
+
+    clone.onerror = () => {
+      this.onLoadError(href);
+    };
+
+    if (this.cleanTimeout) clearTimeout(this.cleanTimeout);
+    this.cleanTimeout = setTimeout(this.onCleanTimeout, CLEANUP_DEBOUNCE_TIME);
+  }
+
+  getClonedCssTextIfAvailable(href: string) {
+    if (
+      href in this.clones &&
+      this.clones[href] !== undefined &&
+      this.clones[href].loaded === true
+    ) {
+      console.log(
+        'AsyncStylesheetManager, getClonedCssTextIfAvailable: returning cloned cssText!',
+      );
+
+      return this.clones[href].cssText;
+    }
+    return null;
+  }
+}
+
+const asyncStylesheetManager = new AsyncStylesheetManager();
+
+export default asyncStylesheetManager;
