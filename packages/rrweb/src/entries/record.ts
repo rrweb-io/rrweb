d2c005fb6e1d6ab4109cd372ea8650ddaae08005
Eoghan Murray <eoghan@getthere.ie>
2026-02-06 12:02:11 +0000

    Fixes to ensure postcss doesn't get bundled into the rrweb-record js output - postcss is only needed at record time

--- packages/rrweb/src/entries/record.ts
+++ packages/rrweb/src/entries/record.ts
@@ -1,3 +1,6 @@
-import record from '../record';
-
-export { record };
+export { default as record } from '../record';
+export {
+  getWindowScroll,
+  getWindowHeight,
+  getWindowWidth,
+} from '../utils';
